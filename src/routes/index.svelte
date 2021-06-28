b2256d981792a891c73c533fa81491f0af5bb2c0
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2021-06-28 15:18:06 +0000

    Prepare for env-based api URL on Scalingo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,33 @@
 <script>
   import { onMount } from "svelte";
-  let msg = [];
+  let msg = null;
 
+  const api_url = import.meta.env.VITE_API_URL || "http://localhost:8000";
   onMount(async () => {
-    const res = await fetch(`http://localhost:8000/hello/`);
+    const res = await fetch(`${api_url}/hello/`);
     const jsonResult = await res.json();
     console.log(jsonResult);
     msg = jsonResult.message;
   });
 </script>
 
-<h1>Dora</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<h1>Hello Dora !</h1>
 
 <div>
   {#if msg}
-    {msg}
+    Le backend dit : <span
+      style="background-color: grey; color: white; padding: 0.2em;">{msg}</span
+    >
   {:else}
-    <!-- this block renders when photos.length === 0 -->
-    <p>loading...</p>
+    <p>Le backend n'est pas connecté</p>
   {/if}
+  <pre>
+    App Mode {import.meta.env.MODE}
+    Base URL {import.meta.env.BASE_URL}
+    PROD {import.meta.env.PROD}
+    DEV {import.meta.env.DEV}
+    API_URL {import.meta.env.VITE_API_URL}
+    DJANGO_ALLOWED_HOSTS {import.meta.env.DJANGO_ALLOWED_HOSTS}
+
+  </pre>
 </div>
