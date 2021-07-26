6491f3bdd569bc541cba7f437923f077e2ac2406
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2021-07-26 17:25:53 +0200

    Update adapter-node version

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import preprocess from "svelte-preprocess";
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from "@sveltejs/adapter-node";
+import node from "@sveltejs/adapter-node";
 
 const config = {
   kit: {
@@ -17,7 +17,7 @@ const config = {
         },
       },
     },
-    adapter: adapter({
+    adapter: node({
       // default options are shown
       out: "build",
       precompress: false,
