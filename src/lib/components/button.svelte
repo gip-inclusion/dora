7d0b8b41f3c426975e8d46277772b81cc18361da
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2021-07-30 10:37:06 +0200

    Improve Button component

--- src/lib/components/button.svelte
+++ src/lib/components/button.svelte
@@ -5,25 +5,51 @@
   export let iconOnRight;
   export let type = "button";
   export let disabled;
+  export let small;
+  export let big;
+
+  let px, py, iw, ih, ts, lead;
+  if (small) {
+    px = "px-5/4";
+    py = "py-3/4";
+    iw = "w-3";
+    ih = "h-3";
+    ts = "text-sm";
+    lead = "leading-normal";
+  } else if (big) {
+    px = "px-3";
+    py = "py-2";
+    iw = "w-4";
+    ih = "h-4";
+    ts = "text-base";
+    lead = "leading-normal";
+  } else {
+    px = "px-5/2";
+    py = "py-3/2";
+    iw = "w-3";
+    ih = "h-3";
+    ts = "text-base";
+    lead = "leading-normal";
+  }
 </script>
 
 <button
   {type}
-  class="flex flex-row justify-center text-base leading-normal text-center text-white rounded py-3/2 px-5/2 bg-dora-magenta-cta hover:bg-dora-magenta-hover disabled:bg-gray-01 disabled:text-gray-text active:bg-france-blue "
+  class="{px} {py} {ts} {lead} flex flex-row items-center justify-center text-center text-white rounded bg-dora-magenta-cta hover:bg-dora-magenta-hover disabled:bg-gray-01 disabled:text-gray-text active:bg-france-blue focus:shadow-focus outline-none "
   {disabled}>
   {#if iconOnLeft}
-    <div class="w-3 h-3 mr-1 fill-current ">
+    <div class="{iw} {ih} mr-1 fill-current ">
       {@html icon}
     </div>
   {/if}
   {#if icon && !label}
-    <div class="w-3 h-3 fill-current ">
+    <div class="{iw} {ih} fill-current ">
       {@html icon}
     </div>
   {/if}
   {label}
   {#if iconOnRight}
-    <div class="w-3 h-3 ml-1 fill-current">
+    <div class="{iw} {ih} ml-1 fill-current">
       {@html icon}
     </div>
   {/if}
