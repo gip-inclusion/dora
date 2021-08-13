bfe270f551fc6abd9eb043ba1419c3c96c1618f0
Cedric Rossi <cedric@terramater.net>
2021-08-13 17:51:04 +0200

    Simplify homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,13 +3,13 @@
   import LinkButton from "$lib/components/link-button.svelte";
 </script>
 
-<CenteredGrid class1="row-start-1">
-  <div class="col-start-1 col-span-full">
-    <LinkButton label="Design System" to="design-system" noBackground />
+<CenteredGrid class="row-start-1">
+  <div class="col-start-1 col-span-full mb-5">
+    <!-- <LinkButton label="Design System" to="design-system" noBackground /> -->
     <LinkButton
-      label="Création de structure"
+      label="Créer une structure"
       to="/structures/creer"
       noBackground />
-    <LinkButton label="Création de service" to="/services/creer" noBackground />
+    <LinkButton label="Créer un service" to="/services/creer" noBackground />
   </div>
 </CenteredGrid>
