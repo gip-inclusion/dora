c1e5338194b291ebfd12c7b8f602c27b274d9ec6
Cedric Rossi <cedric@terramater.net>
2021-09-14 10:02:45 +0200

    Ask the user to continue its last draft instead of saving to localstorage

--- src/routes/services/form/_stores.js
+++ src/routes/services/form/_stores.js
@@ -1,7 +1,4 @@
-import { browser } from "$app/env";
-
 import serviceSchema from "$lib/schemas/service";
-import { storageKey } from "./_constants";
 
 const defaultServiceCache = Object.fromEntries(
   Object.entries(serviceSchema).map(([fieldName, props]) => [
@@ -10,20 +7,6 @@ const defaultServiceCache = Object.fromEntries(
   ])
 );
 
-export function resetServiceCache() {
-  localStorage.removeItem(storageKey);
-}
-
-export function persistServiceCache(service) {
-  localStorage.setItem(storageKey, JSON.stringify(service));
-}
-
 export function getNewService() {
-  if (browser) {
-    const lsContent = localStorage.getItem(storageKey);
-    if (lsContent) {
-      return JSON.parse(lsContent);
-    }
-  }
   return JSON.parse(JSON.stringify(defaultServiceCache));
 }
