1229ee1eac59cd204e189c369b7561f80d1b9981
Cedric Rossi <cedric@terramater.net>
2021-09-21 13:04:41 +0200

    XSS / clickjacking mitigation

--- src/hooks.js
+++ src/hooks.js
@@ -3,3 +3,16 @@ import * as Sentry from "@sentry/browser";
 export async function handleError({ error, request }) {
   Sentry.captureException(error, { request });
 }
+
+export async function handle({ request, resolve }) {
+  const response = await resolve(request);
+
+  return {
+    ...response,
+    headers: {
+      ...response.headers,
+      "X-Frame-Options": "DENY",
+      "X-XSS-Protection": "1; mode=block",
+    },
+  };
+}
