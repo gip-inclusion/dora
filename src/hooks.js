2e780ad6a2ec566c18dd8b79afddac9658ed56f1
Cedric Rossi <cedric@terramater.net>
2021-09-21 14:19:28 +0200

    Add basic CSP headers

--- src/hooks.js
+++ src/hooks.js
@@ -1,3 +1,4 @@
+import { API_URL } from "$lib/env";
 // import * as Sentry from "@sentry/browser";
 
 // export async function handleError({ error, request }) {
@@ -14,6 +15,7 @@ export async function handle({ request, resolve }) {
       "X-Frame-Options": "DENY",
       "X-XSS-Protection": "1; mode=block",
       "X-Content-Type-Options": "nosniff",
+      "Content-Security-Policy": `default-src 'none'; connect-src ${API_URL} https://api-adresse.data.gouv.fr/search/ https://plausible.io/api/event; font-src 'self'; img-src 'self' data:; script-src 'self' 'unsafe-inline' https://plausible.io/js/plausible.js; style-src 'self' 'unsafe-inline'`,
     },
   };
 }
