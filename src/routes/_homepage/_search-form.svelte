4d5143ee993b49c2cfee328e9a22fed1d4d303b9
Cedric Rossi <cedric@terramater.net>
2021-09-30 19:48:26 +0200

    Add warning label above search

--- src/routes/_homepage/_search-form.svelte
+++ src/routes/_homepage/_search-form.svelte
@@ -36,8 +36,16 @@
   .wrapper {
     padding: 24px 24px 16px;
     background-color: var(--col-white);
-    border-radius: var(--s8);
+    border-radius: 0 0 var(--s8) var(--s8);
     box-shadow: var(--shadow-l);
+  }
+
+  .wrapper-top {
+    padding: 24px 24px 24px;
+    background-color: var(--col-magenta-dark);
+    border-radius: var(--s8) var(--s8) 0 0;
+    box-shadow: var(--shadow-l);
+    color: var(--col-white);
   }
 
   form {
@@ -54,6 +62,16 @@
     text-align: center;
   }
 </style>
+
+<div class="wrapper-top">
+  ⚠️ Le service DORA est actuellement <a
+    class="underline"
+    target="_blank"
+    rel="noopener"
+    href="https://beta.gouv.fr/startups/dora.html">en construction</a
+  >, la recherche concerne uniquement ces 3 territoires : Loire-Atlantique,
+  Ardennes et La Réunion.
+</div>
 
 <div class="wrapper">
   <form on:submit|preventDefault={handleSearch}>
@@ -98,14 +116,4 @@
       small
       preventDefaultOnMouseDown />
   </form>
-  <p>
-    Le service DORA est actuellement <a
-      class="underline"
-      target="_blank"
-      rel="noopener"
-      href="https://beta.gouv.fr/startups/dora.html">en construction</a
-    >, et se concentre sur 3 thématiques de services (mobilité, garde d’enfant
-    et hébergement/logement) et 3 territoires (Loire-Atlantique, Ardennes et La
-    Réunion).
-  </p>
 </div>
