8990fe8285148999b68fdae4a167bcff7c7e6ee7
Cedric Rossi <cedric@terramater.net>
2021-11-10 17:29:57 +0100

    Fix CSP after domain change

--- src/hooks.js
+++ src/hooks.js
@@ -15,8 +15,8 @@ export async function handle({ request, resolve }) {
   const connectSrc = `connect-src ${API_URL} ${
     dev ? "ws:" : ""
   } https://api-adresse.data.gouv.fr/search/ https://plausible.io/api/event https://*.sentry.io http://*.hotjar.com:* https://*.hotjar.com:* http://*.hotjar.io https://*.hotjar.io wss://*.hotjar.com`;
-  const scriptSrc = `script-src 'self' 'unsafe-inline' https://metabase.dora.beta.gouv.fr/app/iframeResizer.js https://plausible.io/js/plausible.outbound-links.js https://static.hotjar.com https://script.hotjar.com http://*.hotjar.com https://*.hotjar.com http://*.hotjar.io https://*.hotjar.io`;
-  const frameSrc = `frame-src http://metabase.dora.beta.gouv.fr https://*.hotjar.com http://*.hotjar.io https://*.hotjar.io`;
+  const scriptSrc = `script-src 'self' 'unsafe-inline' https://metabase.dora.fabrique.social.gouv.fr/app/iframeResizer.js https://plausible.io/js/plausible.outbound-links.js https://static.hotjar.com https://script.hotjar.com http://*.hotjar.com https://*.hotjar.com http://*.hotjar.io https://*.hotjar.io`;
+  const frameSrc = `frame-src http://metabase.dora.fabrique.social.gouv.fr https://*.hotjar.com http://*.hotjar.io https://*.hotjar.io`;
   const fontSrc = `font-src 'self' http://*.hotjar.com https://*.hotjar.com http://*.hotjar.io https://*.hotjar.io`;
   const imgSrc = `img-src 'self' data: http://*.hotjar.com https://*.hotjar.com http://*.hotjar.io https://*.hotjar.io`;
   return {
