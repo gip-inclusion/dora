960e58e2f0da6e89fa800b75b788495550ed74c2
Cedric Rossi <cedric@terramater.net>
2022-01-17 15:08:42 +0100

    Refacto <Member> dans le tableau de bord

--- src/routes/tableau-de-bord/structures/[slug]/_member_invited.svelte
+++ src/routes/tableau-de-bord/structures/[slug]/_member_invited.svelte
@@ -1,19 +1,16 @@
 <script>
-  // import { userInfo } from "$lib/auth";
-
-  import ButtonMenu from "$lib/components/button-menu.svelte";
   import Button from "$lib/components/button.svelte";
   import Label from "$lib/components/label.svelte";
 
-  import { fileEditIcon, fileForbidIcon, userIcon, moreIcon } from "$lib/icons";
+  import { fileEditIcon, fileForbidIcon, userIcon } from "$lib/icons";
   import { resendInvite, cancelInvite } from "$lib/structures";
-  import ChangeUserModal from "./_change-user-modal.svelte";
+  import Member from "./_member.svelte";
 
   export let member;
   export let onRefresh;
 
-  let changeUserModalIsOpen = false;
   $: userLevel = member.isAdmin ? "Admin" : "Utilisateur";
+
   async function handleCancelInvite() {
     if (
       confirm(
@@ -24,69 +21,49 @@
       await onRefresh();
     }
   }
+
   async function handleResendInvite() {
     await resendInvite(member.id);
   }
 </script>
 
-<style>
-  .wrapper {
-    display: flex;
-    align-items: center;
-    padding: 6px 16px;
-    background-color: var(--col-white);
-    border-radius: var(--s8);
-    box-shadow: var(--shadow-sm);
-    gap: var(--s16);
-  }
-
-  .is-own {
-    background-color: var(--col-gray-01);
-  }
-</style>
-
-<ChangeUserModal bind:isOpen={changeUserModalIsOpen} bind:member {onRefresh} />
-<div class="wrapper">
-  <div class="flex flex-col">
-    <h5>{member.user.fullName}</h5>
-    <div class="text-gray-text-alt text-f14">{member.user.email}</div>
+<Member {member}>
+  <div slot="label">
+    <Label
+      label={`${userLevel} – Invitation envoyée`}
+      smallIcon
+      iconOnLeft
+      icon={userIcon}
+      light
+    />
   </div>
-  <div class="grow" />
-  <Label
-    label={`${userLevel} – Invitation envoyée`}
-    smallIcon
-    iconOnLeft
-    icon={userIcon}
-    light
-  />
-  <div>
-    <ButtonMenu icon={moreIcon} let:onClose={onCloseParent}>
-      <div>
-        <Button
-          label="Relancer"
-          on:click={() => {
-            handleResendInvite();
-            onCloseParent();
-          }}
-          icon={fileEditIcon}
-          iconOnRight
-          small
-          noBackground
-        />
-      </div>
-      <div>
-        <Button
-          label="Révoquer"
-          on:click={() => {
-            handleCancelInvite();
-            onCloseParent();
-          }}
-          icon={fileForbidIcon}
-          iconOnRight
-          small
-          noBackground
-        />
-      </div>
-    </ButtonMenu>
+
+  <div slot="actions" let:onCloseParent>
+    <div>
+      <Button
+        label="Relancer"
+        on:click={() => {
+          handleResendInvite();
+          onCloseParent();
+        }}
+        icon={fileEditIcon}
+        iconOnRight
+        small
+        noBackground
+      />
+    </div>
+    <div>
+      <Button
+        label="Révoquer"
+        on:click={() => {
+          handleCancelInvite();
+          onCloseParent();
+        }}
+        icon={fileForbidIcon}
+        iconOnRight
+        small
+        noBackground
+      />
+    </div>
   </div>
-</div>
+</Member>
