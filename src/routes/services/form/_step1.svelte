e4fd4de18a214a899ab3a9b4f78cda43d20410d8
Cedric Rossi <cedric@terramater.net>
2022-02-14 08:24:45 +0100

    Supprime la notification sur le périmètre de test dans le formulaire de saisie
    https://trello.com/c/OVexs82a/230-supprimer-le-texte-périmètre-de-test-du-formulaire-de-saisie-de-services

--- src/routes/services/form/_step1.svelte
+++ src/routes/services/form/_step1.svelte
@@ -5,8 +5,6 @@
   import { formErrors } from "$lib/validation.js";
   import serviceSchema from "$lib/schemas/service.js";
 
-  import Info from "$lib/components/forms/form-info.svelte";
-
   export let servicesOptions, service, structures;
   let subcategories = [];
 
@@ -56,12 +54,6 @@
 {/if}
 
 <FieldSet title="Typologie de service">
-  <Info title="Périmètre de test">
-    Dans un premier temps, seuls les services liés aux freins périphériques :
-    <strong>mobilités</strong>, <strong>garde d'enfant</strong> et
-    <strong>hébergement / logement</strong> sont testés.
-  </Info>
-
   <ModelField
     type="select"
     label="Thématique"
