5d1e81ddaef9ed48bb9d66b6a2906a4a02b9e959
Alexandre CANTIN <cantin.alexandre@gmail.com>
2022-10-12 19:10:46 +0200

    Pas de PDF pour les services non publiés (#124)

--- src/lib/components/services/body/service-share.svelte
+++ src/lib/components/services/body/service-share.svelte
@@ -6,6 +6,7 @@
   import { checkIcon, downloadIcon, linkIcon } from "$lib/icons";
 
   import type { DashboardService, Service } from "$lib/types";
+  import { SERVICE_STATUSES } from "$lib/types";
   import { trackPDFDownload } from "$lib/utils/plausible";
 
   export let service: Service | DashboardService;
@@ -55,13 +56,15 @@
     </span>
   </button>
 
-  <LinkButton
-    secondary
-    wFull
-    icon={downloadIcon}
-    label="Télécharger en PDF"
-    to={pdfUrl}
-    on:click={() => trackPDFDownload(service)}
-    nofollow
-  />
+  {#if service.status === SERVICE_STATUSES.PUBLISHED}
+    <LinkButton
+      secondary
+      wFull
+      icon={downloadIcon}
+      label="Télécharger en PDF"
+      to={pdfUrl}
+      on:click={() => trackPDFDownload(service)}
+      nofollow
+    />
+  {/if}
 </div>
