16e2922829adaaf1a1db61df1da0de25deee80fb
Cedric Rossi <cedric@terramater.net>
2022-10-18 13:58:09 +0200

    Active les sourcemaps

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 // vite.config.js
 import { sveltekit } from "@sveltejs/kit/vite";
 
-/** @type {import('vite').UserConfig} */
+/** @type {import("vite").UserConfig} */
 const config = {
   plugins: [sveltekit()],
+
+  build: {
+    sourcemap: true,
+  },
 };
 
 export default config;
