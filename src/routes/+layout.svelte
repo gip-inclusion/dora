90bdbfec819fae43af0b3e55ed0bf24cef009658
Cedric Rossi <cedric@terramater.net>
2022-11-25 11:08:21 +0100

    Corrige le /favico.ico manquant

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
   import { ENVIRONMENT } from "$lib/env";
-  import favicoIco from "$lib/assets/favicon.ico";
-  import favicoSvg from "$lib/assets/favicon.svg";
-  import favicoPng from "$lib/assets/favicon.png";
-
   import "../app.postcss";
 
   import Footer from "./_layout/_footer.svelte";
@@ -12,9 +8,6 @@
 </script>
 
 <svelte:head>
-  <link rel="icon" href={favicoIco} sizes="any" />
-  <link rel="icon" href={favicoSvg} type="image/svg+xml" />
-  <link rel="apple-touch-icon" href={favicoPng} />
   {#if ENVIRONMENT !== "production"}
     <meta name="robots" content="noindex" />
   {/if}
