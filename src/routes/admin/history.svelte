4ace5bd7d716de461cee3e7e6b700eaf878c02e5
Cedric Rossi <cedric@terramater.net>
2023-01-20 15:32:02 +0100

    HOTFIX: Corrige l'ouverture de la page admin des structures

--- src/routes/admin/history.svelte
+++ src/routes/admin/history.svelte
@@ -7,7 +7,7 @@
 <div class="max-h-s160 overflow-scroll">
   {#each notes as note}
     <div class="bg-gray-01">
-      <DateLabel date={note.date} /> par {note.user.email}
+      <DateLabel date={note.date} /> par {note.user?.email}
     </div>
     <div class="ml-s32">
       {#each note.message.split("\n") as msg}
