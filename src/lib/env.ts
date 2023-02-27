bfebe28697a99699dde632aa9fc3c76cf3df86b9
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2023-02-27 17:08:11 +0100

    Nettoyage d'une variable d'env. obsolète

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -5,6 +5,5 @@ export const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT;
 export const INTERNAL_API_URL = import.meta.env.VITE_INTERNAL_API_URL;
 export const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
 export const CANONICAL_URL = import.meta.env.VITE_CANONICAL_URL;
-export const CRISP_ID = import.meta.env.VITE_CRISP_ID;
 export const METABASE_EMBED_URL = import.meta.env.VITE_METABASE_EMBED_URL;
 export const PLAUSIBLE_EMBED_URL = import.meta.env.VITE_PLAUSIBLE_EMBED_URL;
