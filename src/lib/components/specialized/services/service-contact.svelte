f0fe50cfc97cc19def4efc8607d14bd5681afe67
Alexandre CANTIN <cantin.alexandre@gmail.com>
2023-07-03 11:29:15 +0200

    Service design fixes (#287)

--- src/lib/components/specialized/services/service-contact.svelte
+++ src/lib/components/specialized/services/service-contact.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import type { Service, ShortService } from "$lib/types";
+  import AbTestingSection from "../ab-testing-section.svelte";
   import ContactEmail from "./contact-email.svelte";
   import ContactPhone from "./contact-phone.svelte";
+  import ServiceContact from "./display/new/service-contact.svelte";
 
   export let service: Service | ShortService;
   export let useWhiteText = false;
@@ -17,55 +19,74 @@
   const nonePreferred = !emailPreferred && !phonePreferred;
 </script>
 
-<div>
-  <div class="flex flex-col gap-s4 text-f14" class:text-white={useWhiteText}>
-    {#if service.contactName}
-      <p class="mb-s6 mr-s24 text-gray-dark" class:text-white={useWhiteText}>
-        <strong>{service.contactName}</strong>
-      </p>
-    {/if}
-    {#if emailPreferred}
-      <ContactEmail {service} preferred />
-      {#if !phonePreferred}
-        <p class="text-f12 text-gray-text" class:text-white={useWhiteText}>
-          Cette structure privilégie l’e-mail comme moyen pour mobiliser ce
-          service.
+<AbTestingSection
+  abTestingName="mobilisation"
+  showIfGroups={["mobilisation--ancien-design"]}
+>
+  <div>
+    <div class="flex flex-col gap-s4 text-f14" class:text-white={useWhiteText}>
+      {#if service.contactName}
+        <p class="mb-s6 mr-s24 text-gray-dark" class:text-white={useWhiteText}>
+          <strong>{service.contactName}</strong>
         </p>
       {/if}
-    {/if}
-    {#if phonePreferred}
-      <ContactPhone {service} preferred />
-      {#if !emailPreferred}
-        <p class="text-f12 text-gray-text">
-          Cette structure privilégie le téléphone comme moyen pour mobiliser ce
-          service.
-        </p>
-      {/if}
-    {/if}
-    {#if nonePreferred}
-      Pensez à consulter les <a href="#orientation-modes" class="underline"
-        >conditions de mobilisation</a
-      >
-    {/if}
-
-    <div class="my-s8 rounded-md bg-info-light p-s16 text-f14 text-gray-text">
-      Pensez à préciser que vous avez identifié le service sur DORA 😇
-    </div>
-
-    {#if !allPreferred}
-      <p class="mb-s6 mr-s24 text-gray-dark" class:text-white={useWhiteText}>
-        {#if nonePreferred}
-          <strong>Coordonnées</strong>
-        {:else}
-          <strong>Autres coordonnées</strong>
+      {#if emailPreferred}
+        <ContactEmail {service} preferred />
+        {#if !phonePreferred}
+          <p class="text-f12 text-gray-text" class:text-white={useWhiteText}>
+            Cette structure privilégie l’e-mail comme moyen pour mobiliser ce
+            service.
+          </p>
         {/if}
-      </p>
-      {#if !emailPreferred && service.contactEmail}
-        <ContactEmail {service} />
       {/if}
-      {#if !phonePreferred && service.contactPhone}
-        <ContactPhone {service} />
+      {#if phonePreferred}
+        <ContactPhone {service} preferred />
+        {#if !emailPreferred}
+          <p class="text-f12 text-gray-text">
+            Cette structure privilégie le téléphone comme moyen pour mobiliser
+            ce service.
+          </p>
+        {/if}
       {/if}
-    {/if}
+      {#if nonePreferred}
+        Pensez à consulter les <a href="#orientation-modes" class="underline"
+          >conditions de mobilisation</a
+        >
+      {/if}
+
+      <div class="my-s8 rounded-md bg-info-light p-s16 text-f14 text-gray-text">
+        Pensez à préciser que vous avez identifié le service sur DORA 😇
+      </div>
+
+      {#if !allPreferred}
+        <p class="mb-s6 mr-s24 text-gray-dark" class:text-white={useWhiteText}>
+          {#if nonePreferred}
+            <strong>Coordonnées</strong>
+          {:else}
+            <strong>Autres coordonnées</strong>
+          {/if}
+        </p>
+        {#if !emailPreferred && service.contactEmail}
+          <ContactEmail {service} />
+        {/if}
+        {#if !phonePreferred && service.contactPhone}
+          <ContactPhone {service} />
+        {/if}
+      {/if}
+    </div>
   </div>
-</div>
+</AbTestingSection>
+
+<AbTestingSection
+  abTestingName="mobilisation"
+  showIfGroups={["mobilisation--fond-bleu"]}
+>
+  <ServiceContact {service} useWhiteText />
+</AbTestingSection>
+
+<AbTestingSection
+  abTestingName="mobilisation"
+  showIfGroups={["mobilisation--fond-blanc"]}
+>
+  <ServiceContact {service} />
+</AbTestingSection>
--- src/routes/(modeles-services)/_common/display/service-login-notice.svelte
+++ src/routes/(modeles-services)/_common/display/service-login-notice.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { page } from "$app/stores";
   import LinkButton from "$lib/components/display/link-button.svelte";
+  import AbTestingSection from "$lib/components/specialized/ab-testing-section.svelte";
+  import { getAbTestingUserGroup } from "$lib/utils/ab-testing";
   import { trackMobilisationLogin } from "$lib/utils/plausible";
 
   export let service;
@@ -10,21 +12,50 @@
   }
 </script>
 
-<div class="flex w-full flex-col rounded-lg bg-info-light p-s16">
-  <strong class="mb-s8 text-f17 text-service-blue-info">
-    Pour mobiliser ce service, vous devez vous connecter&nbsp;!
-  </strong>
-  <div class="mb-s24 text-f14 text-gray-text">
-    En vous connectant, vous aurez accès aux informations de contact afin de
-    mobiliser ce service pour votre bénéficiaire.
+<AbTestingSection
+  abTestingName="mobilisation"
+  showIfGroups={["mobilisation--ancien-design"]}
+>
+  <div class="flex w-full flex-col rounded-lg bg-info-light p-s16">
+    <strong class="mb-s8 text-f17 text-service-blue-info">
+      Pour mobiliser ce service, vous devez vous connecter&nbsp;!
+    </strong>
+    <div class="mb-s24 text-f14 text-gray-text">
+      En vous connectant, vous aurez accès aux informations de contact afin de
+      mobiliser ce service pour votre bénéficiaire.
+    </div>
+
+    <div>
+      <LinkButton
+        on:click={trackClick}
+        small
+        label="Se connecter"
+        to={`/auth/connexion?next=${encodeURIComponent($page.url.pathname)}`}
+      />
+    </div>
+  </div>
+</AbTestingSection>
+
+<AbTestingSection
+  abTestingName="mobilisation"
+  showIfGroups={["mobilisation--fond-bleu", "mobilisation--fond-blanc"]}
+>
+  {@const blueDesign =
+    getAbTestingUserGroup("mobilisation") === "mobilisation--fond-bleu"}
+
+  <div class="mb-s24 text-f14 text-gray-text" class:text-white={blueDesign}>
+    Pour accéder aux informations de contact de ce service, vous devez vous
+    connecter.
   </div>
 
   <div>
     <LinkButton
       on:click={trackClick}
-      small
-      label="Se connecter"
+      extraClass={blueDesign
+        ? "bg-white !text-france-blue hover:!text-white"
+        : ""}
+      label="S‘identifier avec Inclusion Connect"
       to={`/auth/connexion?next=${encodeURIComponent($page.url.pathname)}`}
     />
   </div>
-</div>
+</AbTestingSection>
