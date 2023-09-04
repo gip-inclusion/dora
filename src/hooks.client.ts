31e4ea25436d1afb99d4f658925fb6e817fbaa1a
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2023-09-04 17:16:49 +0200

    Mise à jour Sentry (#331)

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,5 +1,5 @@
+import * as Sentry from "@sentry/sveltekit";
 import { ENVIRONMENT, SENTRY_DSN } from "$lib/env";
-import * as Sentry from "@sentry/browser";
 import type { HandleClientError } from "@sveltejs/kit";
 
 if (ENVIRONMENT !== "local") {
@@ -7,15 +7,17 @@ if (ENVIRONMENT !== "local") {
     dsn: SENTRY_DSN,
     environment: ENVIRONMENT,
     tracesSampleRate: 0,
+    tracePropagationTargets: [],
   });
 }
 
-export const handleError: HandleClientError = ({ error, event }) => {
-  Sentry.captureException(error, { event });
+export const handleError: HandleClientError = Sentry.handleErrorWithSentry(
+  ({ error, _event }) => {
+    const message =
+      ENVIRONMENT === "local" ? error.message : "Erreur inattendue";
 
-  const message = ENVIRONMENT === "local" ? error.message : "Erreur inattendue";
-
-  return {
-    message,
-  };
-};
+    return {
+      message,
+    };
+  }
+);
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,6 @@
+import { sequence } from "@sveltejs/kit/hooks";
+import * as Sentry from "@sentry/sveltekit";
 import { ENVIRONMENT, SENTRY_DSN } from "$lib/env";
-import * as Sentry from "@sentry/svelte";
 import type { Handle, HandleServerError } from "@sveltejs/kit";
 
 if (ENVIRONMENT !== "local") {
@@ -7,29 +8,34 @@ if (ENVIRONMENT !== "local") {
     dsn: SENTRY_DSN,
     environment: ENVIRONMENT,
     tracesSampleRate: 0,
+    tracePropagationTargets: [],
   });
 }
 
-export const handleError: HandleServerError = ({ error, event }) => {
-  Sentry.captureException(error, { event });
+export const handleError: HandleServerError = Sentry.handleErrorWithSentry(
+  ({ error, _event }) => {
+    const message =
+      ENVIRONMENT === "local" ? error.message : "Erreur inattendue";
 
-  const message = ENVIRONMENT === "local" ? error.message : "Erreur inattendue";
-
-  return {
-    message,
-  };
-};
-
-export const handle: Handle = async ({ event, resolve }) => {
-  const response = await resolve(event);
-
-  if (ENVIRONMENT !== "production") {
-    response.headers.set("X-Robots-Tag", "noindex");
+    return {
+      message,
+    };
   }
-  response.headers.set("X-Frame-Options", "DENY");
-  response.headers.set("X-XSS-Protection", "1; mode=block");
-  response.headers.set("X-Content-Type-Options", "nosniff");
-  response.headers.set("Referrer-Policy", "strict-origin");
+);
 
-  return response;
-};
+export const handle: Handle = sequence(
+  Sentry.sentryHandle(),
+  async ({ event, resolve }) => {
+    const response = await resolve(event);
+
+    if (ENVIRONMENT !== "production") {
+      response.headers.set("X-Robots-Tag", "noindex");
+    }
+    response.headers.set("X-Frame-Options", "DENY");
+    response.headers.set("X-XSS-Protection", "1; mode=block");
+    response.headers.set("X-Content-Type-Options", "nosniff");
+    response.headers.set("Referrer-Policy", "strict-origin");
+
+    return response;
+  }
+);
