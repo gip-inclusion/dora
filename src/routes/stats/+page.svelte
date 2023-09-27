4e17c60bff26bce2bb6db9a09243afcd95ac18b4
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2023-09-27 11:50:30 +0200

    Réglages de l'affichage de la page stats
    (cherry picked from commit 176e84bfe24351efef846b742ac7090a83050ca6)

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -6,14 +6,6 @@
   import { METABASE_EMBED_URL } from "$lib/env";
 </script>
 
-<svelte:head />
-
-<CenteredGrid>
-  <div class="text-center">
-    <h1 class="text-france-blue">Statistiques</h1>
-  </div>
-</CenteredGrid>
-
 <CenteredGrid>
   <iframe
     title="Dashboard Metabase"
@@ -21,11 +13,6 @@
     allowtransparency
     on:load={() => iframeResizer({}, "#metabaseIframe")}
     id="metabaseIframe"
+    class="w-full border-0"
   />
 </CenteredGrid>
-
-<style lang="postcss">
-  iframe {
-    @apply w-full border-0;
-  }
-</style>
