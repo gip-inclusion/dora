8649a9390e1ceae7f56d19c7400d1c6a36496e43
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2023-09-28 19:57:22 +0200

    Désactive la mobilisation pour les utilisateurs potentiels

--- src/routes/(modeles-services)/_common/display/service-mobilisation.svelte
+++ src/routes/(modeles-services)/_common/display/service-mobilisation.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
   import { page } from "$app/stores";
+  import { userInfo } from "$lib/utils/auth";
+  import Notice from "$lib/components/display/notice.svelte";
+
   import Button from "$lib/components/display/button.svelte";
   import ServiceContact from "$lib/components/specialized/services/service-contact.svelte";
   import ServiceLoginNotice from "./service-login-notice.svelte";
@@ -25,37 +28,47 @@
 <h2 class="text-f23 text-white">Mobiliser le service</h2>
 
 {#if $token}
-  <div class="w-full sm:w-auto">
-    <div class="hidden print:inline">
-      <ServiceContact {service} />
-    </div>
-    <div class="print:hidden">
-      {#if !contactOpen}
-        <div class="text-white">
-          <Button
-            on:click={handleShowContactClick}
-            extraClass="!bg-france-blue text-white !border !border-white hover:!bg-magenta-cta hover:!border-france-blue"
-            label="Voir les informations de contact"
-            wFull
-          />
-        </div>
-      {:else}
+  {#if !$userInfo.structures.length}
+    <Notice type="warning-dark"
+      >{#if $userInfo.pendingStructures.length === 1}
+        Le temps que votre adhésion à la structure “{$userInfo
+          .pendingStructures[0].name}” soit validée, vous ne pouvez pas
+        visualiser ces informations.{:else}Le temps que vos demandes d’adhésion
+        soient validées, vous ne pouvez pas visualiser ces informations.{/if}
+    </Notice>
+  {:else}
+    <div class="w-full sm:w-auto">
+      <div class="hidden print:inline">
         <ServiceContact {service} />
-      {/if}
+      </div>
+      <div class="print:hidden">
+        {#if !contactOpen}
+          <div class="text-white">
+            <Button
+              on:click={handleShowContactClick}
+              extraClass="!bg-france-blue text-white !border !border-white hover:!bg-magenta-cta hover:!border-france-blue"
+              label="Voir les informations de contact"
+              wFull
+            />
+          </div>
+        {:else}
+          <ServiceContact {service} />
+        {/if}
 
-      {#if service.isOrientable}
-        <div class="mb-s16 mt-s16">
-          <LinkButton
-            nofollow
-            to="/services/{service.slug}/orienter"
-            extraClass="bg-white !text-france-blue hover:!text-white text-center !whitespace-normal text-center"
-            label="Orienter un ou une bénéficiaire"
-            wFull
-          />
-        </div>
-      {/if}
+        {#if service.isOrientable}
+          <div class="mb-s16 mt-s16">
+            <LinkButton
+              nofollow
+              to="/services/{service.slug}/orienter"
+              extraClass="bg-white !text-france-blue hover:!text-white text-center !whitespace-normal text-center"
+              label="Orienter un ou une bénéficiaire"
+              wFull
+            />
+          </div>
+        {/if}
+      </div>
     </div>
-  </div>
+  {/if}
 {:else}
   <ServiceLoginNotice {service} />
 {/if}
