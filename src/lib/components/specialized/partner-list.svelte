1b71a570169ff940098c1cd7150d71b20130f5dd
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2023-11-28 15:12:05 +0100

    Supprime temporairement les logos partenaires pour faciliter la detection Open Graph

--- src/lib/components/specialized/partner-list.svelte
+++ src/lib/components/specialized/partner-list.svelte
@@ -11,7 +11,7 @@
       class="m-s0 inline"
       class:max-w-[200px]={imgHeight === "medium"}
       class:max-w-[150px]={imgHeight === "small"}
-      src={partner.img}
+      _src={partner.img}
       alt={partner.name}
     />
   </li>
