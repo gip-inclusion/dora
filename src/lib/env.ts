3f8be9151148defa84816e47d9824db63922bad0
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2023-11-28 10:11:28 +0100

    Ajout des balises Open Graph

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,6 +4,6 @@ export const PDF_SERVICE_URL =
 export const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT;
 export const INTERNAL_API_URL = import.meta.env.VITE_INTERNAL_API_URL;
 export const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
-export const CANONICAL_URL = import.meta.env.VITE_CANONICAL_URL;
+export const CANONICAL_URL = import.meta.env.VITE_PUBLIC_CANONICAL_URL;
 export const METABASE_EMBED_URL = import.meta.env.VITE_METABASE_EMBED_URL;
 export const FLAG_STRIKING = import.meta.env.VITE_FLAG_STRIKING === "true";
