f7e5e62d6f118306b6c38bb50a260d85b0291d6b
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2024-02-14 12:03:42 +0100

    Correction du formulaire d’édition de modèles
    https://trello.com/c/DPhg3QgP/1012-wordings-formulaires-de-services-enlever-les-placeholders-des-champs-de-saisie

--- src/routes/(modeles-services)/modeles/model-edition-form.svelte
+++ src/routes/(modeles-services)/modeles/model-edition-form.svelte
@@ -98,14 +98,14 @@
   <CenteredGrid>
     <div class="lg:w-2/3">
       {#if model?.structure}
-        <FieldsTypology bind:service={model} {servicesOptions} {model} />
-
-        <FieldsPresentation
+        <FieldsTypology
           noTopPadding
           bind:service={model}
           {servicesOptions}
           {model}
         />
+
+        <FieldsPresentation bind:service={model} {servicesOptions} {model} />
 
         <FieldsPublics bind:service={model} {servicesOptions} {model} />
 
