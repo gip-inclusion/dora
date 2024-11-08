d9cd46f1355c203d19f32059abe23de0073e80af
Jérémy Buget <jbuget@gmail.com>
2024-11-08 17:35:50 +0100

    feat: add script to import Cresus structures and services

---
+++ back/scripts/import_cresus_services.py
@@ -0,0 +1,127 @@
+from types import SimpleNamespace
+import csv
+import sys
+from django.db import transaction
+from dora.structures.models import Structure
+from dora.services.models import ServiceModel, ServiceSource
+from dora.services.utils import instantiate_model
+from dora.users.models import User
+
+csv_file_path = "./cresus_services.csv"
+
+# 💡 Mettre à True pour activer les écritures en base de données
+dry_run = True
+
+created_count = 0
+error_count = 0
+
+bot_user = User.objects.get_dora_bot()
+source, _ = ServiceSource.objects.get_or_create(
+    value=f"fichier-cresus",
+    defaults={"label": "Fichier CSV des services de la fondation Cresus"},
+)
+
+
+def _extract_data_from_line(line):
+    data = SimpleNamespace(
+        modele_slug=line.get("modele_slug").strip(),
+        structure_name=line.get("structure_name").strip(),
+        structure_siret=line.get("structure_siret").replace(" ", "").strip(),
+        contact_name=line.get("contact_name").strip(),
+        contact_email=line.get("contact_email").strip(),
+        contact_phone=line.get("contact_phone").replace(" ", "").strip(),
+        location_kind=line.get("location_kind").strip(),
+        location_city=line.get("location_city").strip(),
+        location_address=line.get("location_address").strip(),
+        location_complement=line.get("location_complement").strip(),
+        location_postal_code=line.get("location_postal_code").replace(" ", "").strip(),
+        diffusion_zone_type=line.get("diffusion_zone_type").strip(),
+        diffusion_zone_city=line.get("diffusion_zone_city").strip(),
+    )
+    return data
+
+
+def _edit_and_save_service(service, data):
+    service.creator = bot_user
+    service.last_editor = bot_user
+    service.source = source
+    service.contact_name = data.contact_name
+    service.contact_email = data.contact_email
+    service.contact_phone = data.contact_phone
+    service.address1 = data.location_address
+    service.address2 = data.location_complement
+    service.postal_code = data.location_postal_code
+    service.save()
+
+
+if dry_run:
+    print("🧘 DRY RUN 🧘")
+else:
+    print("⚠️ PRODUCTION RUN ⚠️")
+
+with open(csv_file_path, "r") as f:
+    rdr = csv.reader(f)
+    [headers, *lines] = rdr
+    lines = [dict(zip(headers, line)) for line in lines]
+
+try:
+    with transaction.atomic():
+        for idx, line in enumerate(lines, 1):
+            try:
+                print(f"\nTraitement de la ligne {idx} :")
+
+                # Récupération des données
+                data = _extract_data_from_line(line)
+
+                # Vérification que le SIRET de la structure est bien renseigné
+                if not data.structure_siret:
+                    print(f"\tSIRET manquant. Ligne ignorée.", file=sys.stderr)
+                    error_count += 1
+                    continue
+
+                # Vérification que la structure existe bien en BDD
+                try:
+                    structure = Structure.objects.get(siret=data.structure_siret)
+                except Structure.DoesNotExist:
+                    print(
+                        f"\tStructure avec le SIRET {data.structure_siret} introuvable. Ligne ignorée.",
+                        file=sys.stderr,
+                    )
+                    error_count += 1
+                    continue
+
+                # Vérification que le modèle de service existe lui aussi
+                try:
+                    model = ServiceModel.objects.get(slug=data.modele_slug)
+                except ServiceModel.DoesNotExist:
+                    print(
+                        f"\tModèle de service avec le slug {data.modele_slug} introuvable. Ligne ignorée.",
+                        file=sys.stderr,
+                    )
+                    error_count += 1
+                    continue
+
+                # Création du service
+                print(f"\tCréation d'un nouveau service pour la structure avec le SIRET '{data.structure_siret}'.")
+                new_service = instantiate_model(model, structure, bot_user)
+                _edit_and_save_service(new_service, data)
+                created_count += 1
+                print(f"\tService créé.")
+
+            except Exception as e:
+                print(f"\tErreur lors du traitement - {e}", file=sys.stderr)
+                error_count += 1
+                continue
+
+        # Forcer un rollback si dry_run est activé
+        if dry_run:
+            raise Exception("Mode dry-run activé. Toutes les modifications sont annulées.")
+
+except Exception as e:
+    if str(e) != "Mode dry-run activé. Toutes les modifications sont annulées.":
+        print(f"Erreur critique : {e}", file=sys.stderr)
+
+print("\n--------------------------------------------------")
+print("Traitement du fichier CSV terminé.")
+print(f"Résumé : {created_count} services créés, {error_count} erreurs.")
+print("--------------------------------------------------\n")
---
+++ back/scripts/import_cresus_structures.py
@@ -0,0 +1,177 @@
+from types import SimpleNamespace
+import csv
+import sys
+from django.db import transaction
+from dora.structures.models import (
+    Structure,
+    StructureMember,
+    StructurePutativeMember,
+    StructureSource,
+)
+from dora.structures.emails import send_invitation_email
+from dora.sirene.models import Establishment
+from dora.users.models import User
+from dora.core.validators import validate_siret, ValidationError
+
+# 💡 Mettre à True pour activer les écritures en base de données
+dry_run = True
+
+csv_file_path = "./cresus_structures.csv"
+
+created_count = 0
+ignored_count = 0
+error_count = 0
+
+bot_user = User.objects.get_dora_bot()
+source, _ = StructureSource.objects.get_or_create(
+    value=f"fichier-cresus",
+    defaults={"label": "Fichier CSV des structures de la fondation Cresus"},
+)
+
+
+def _extract_data_from_line(line):
+    data = SimpleNamespace(
+        name=line.get("name").strip(),
+        siret=line.get("siret").replace(" ", "").strip(),
+        admin_email=line.get("admin_email").strip(),
+        structure_email=line.get("structure_email").strip(),
+        phone_number=line.get("phone_number").replace(" ", "").strip(),
+        label=line.get("label").strip(),
+        website=line.get("website").strip(),
+        location_city=line.get("location_city").strip(),
+        location_address=line.get("location_address").strip(),
+        location_complement=line.get("location_complement").strip(),
+        location_postal_code=line.get("location_postal_code").replace(" ", "").strip(),
+    )
+    return data
+
+
+def _edit_and_save_structure(structure, data):
+    structure.creator = bot_user
+    structure.last_editor = bot_user
+    structure.source = source
+    structure.name = data.name
+    structure.email = data.structure_email
+    structure.phone = data.phone_number
+    structure.url = data.website
+    structure.save()
+
+
+def _invite_structure_admin(structure, email):
+    try:
+        user = User.objects.get(email=email)
+    except User.DoesNotExist:
+        user = User.objects.create_user(email)
+
+    try:
+        member = StructurePutativeMember.objects.get(user=user, structure=structure)
+        print(f"{email} a déjà été invité·e")
+        if not member.is_admin:
+            member.is_admin = True
+            member.save()
+    except StructurePutativeMember.DoesNotExist:
+        try:
+            member = StructureMember.objects.get(user=user, structure=structure)
+            print(f"{email} est déjà membre de la structure")
+
+            if not member.is_admin:
+                member.is_admin = True
+                member.save()
+        except StructureMember.DoesNotExist:
+            member = StructurePutativeMember.objects.create(
+                user=user,
+                structure=structure,
+                invited_by_admin=True,
+                is_admin=True,
+            )
+
+            print(f"{email} invité·e comme administrateur·rice")
+            send_invitation_email(member, "L’équipe DORA")
+
+
+if dry_run:
+    print("🧘 DRY RUN 🧘")
+else:
+    print("⚠️ PRODUCTION RUN ⚠️")
+
+with open(csv_file_path, "r") as f:
+    rdr = csv.reader(f)
+    [headers, *lines] = rdr
+    lines = [dict(zip(headers, line)) for line in lines]
+
+try:
+    with transaction.atomic():
+        for idx, line in enumerate(lines, 1):
+            try:
+                print(f"\nTraitement de la ligne {idx + 1} :")
+
+                # Récupération des données
+                data = _extract_data_from_line(line)
+
+                # Vérification que le SIRET est renseigné
+                if not data.siret:
+                    print(
+                        f"\tSIRET manquant ou vide. Ligne ignorée.",
+                        file=sys.stderr,
+                    )
+                    error_count += 1
+                    continue
+
+                # Vérification que le SIRET est valide
+                try:
+                    validate_siret(data.siret)
+                except ValidationError as e:
+                    print(
+                        f"\tSIRET invalide ({data.siret}) - {e}. Ligne ignorée.",
+                        file=sys.stderr,
+                    )
+                    error_count += 1
+                    continue
+
+                # Vérification que la structure n'existe pas déjà
+                structure = Structure.objects.filter(siret=data.siret).first()
+                if structure:
+                    print(f"\tStructure existante : '{structure.name}'. Ligne ignorée.")
+                    ignored_count += 1
+                    continue
+
+                # Création de la structure
+                print(f"\tCréation d'une nouvelle structure pour le SIRET {data.siret}")
+                try:
+                    establishment = Establishment.objects.get(siret=data.siret)
+                    print(
+                        f"\tÉtablissement trouvé pour le SIRET {data.siret}. Création à partir de celui-ci."
+                    )
+                    structure = Structure.objects.create_from_establishment(
+                        establishment, data.name
+                    )
+                except Establishment.DoesNotExist:
+                    print(
+                        f"\tAucun établissement trouvé pour le SIRET {data.siret}. Création à partir des données CSV."
+                    )
+                    structure = Structure(name=data.name, siret=data.siret)
+
+                _edit_and_save_structure(structure, data)
+                _invite_structure_admin(structure, data.admin_email)
+                created_count += 1
+                print(f"\tStructure créée.")
+
+            except Exception as e:
+                print(f"\tErreur inattendue - {e}", file=sys.stderr)
+                error_count += 1
+                continue
+
+        # Forcer un rollback si dry_run est activé
+        if dry_run:
+            raise Exception("Mode dry-run activé. Toutes les modifications sont annulées.")
+
+except Exception as e:
+    if str(e) != "Mode dry-run activé. Toutes les modifications sont annulées.":
+        print(f"Erreur critique : {e}", file=sys.stderr)
+
+print("\n--------------------------------------------------")
+print("Traitement du fichier CSV terminé.")
+print(
+    f"Résumé : {created_count} structures créées, {ignored_count} structures ignorées, {error_count} erreurs."
+)
+print("--------------------------------------------------\n")
