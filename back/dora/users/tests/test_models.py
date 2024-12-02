3e1e6fc8e4d9dfa8587882379aeeca0c31c81d84
ikarius <fred@ikarius.com>
2024-12-02 16:01:10 +0100

    Redirection vers le rattachement à la connexion (si nécessaire) (#46)
    * model: ajout d'une méthode utilitaire sur le modèle `User`
    
    Permet de savoir si un utilisateur doit être redirigé vers un
    rattachement de structure ou d'agence FT à la connexion.
    
    * oidc: modification du backend pour transmettre le SIRET / SAFIR
    
    * views: transmission du SIRET / SAFIR au frontend si nécessaire
    
    Si un rattachement à un SIRET ou un SAFIR est nécessaire, les
    informations sont transmises au frontend pour une redirection vers la
    fenêtre de rattachement (qui sera pré-remplie).
    
    * auth: redirection vers le rattachement de structure si nécessaire
    
    * tests: ajout de tests sur le rattachement des utilisateurs après connexion
    
    * revue: renommage d'ume méthode de User
    
    * fix: rattrapage du backend
    
    Incorrectement mis à jour par le commit de merge

---
+++ back/dora/users/tests/test_models.py
@@ -0,0 +1,40 @@
+from dora.core.test_utils import make_structure, make_user
+
+
+def test_structure_to_join():
+    user = make_user()
+
+    assert user.structure_to_join(
+        siret="12345678901234"
+    ), "L'utilisateur devrait pouvoir rejoindre la structure"
+    assert user.structure_to_join(
+        safir="02012",
+    ), "L'utilisateur devrait pouvoir rejoindre cette agence FT"
+
+
+def test_should_not_join_structure():
+    user = make_user()
+    structure = make_structure(putative_member=user)
+
+    assert not user.structure_to_join(
+        siret=structure.siret
+    ), "L'utilisateur ne devrait pas pouvoir rejoindre la structure (invité)"
+
+    structure = make_structure(user=user)
+    assert not user.structure_to_join(
+        siret=structure.siret
+    ), "L'utilisateur ne devrait pas pouvoir rejoindre la structure (membre)"
+
+
+def test_should_not_join_ft_agency():
+    user = make_user()
+    make_structure(putative_member=user, code_safir_pe="02012")
+
+    assert not user.structure_to_join(
+        safir="02012"
+    ), "L'utilisateur ne devrait pas pouvoir rejoindre cette agence (invité)"
+
+    make_structure(user=user, code_safir_pe="02013")
+    assert not user.structure_to_join(
+        safir="02013"
+    ), "L'utilisateur ne devrait pas pouvoir rejoindre cette agence (membre)"
