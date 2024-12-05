e43d1c783fc1b2889abdae94b0171e37f2a6dbdd
Jérémy Buget <jbuget@gmail.com>
2024-12-05 14:37:43 +0100

    front: ajout d'une section dans l'interface d'édition d'un service pour saisir la durée de prestation

---
+++ front/src/lib/components/specialized/services/fields-duration.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import FieldSet from "$lib/components/display/fieldset.svelte";
+  import BasicInputField from "$lib/components/forms/fields/basic-input-field.svelte";
+  import type { Service } from "$lib/types";
+
+  export let service: Service;
+</script>
+
+<FieldSet title="Durée de la prestation">
+  <BasicInputField
+    type="number"
+    id="durationWeeklyHours"
+    description="En nombre d'heures"
+    bind:value={service.durationWeeklyHours}
+  />
+
+  <BasicInputField
+    type="number"
+    id="durationWeeks"
+    description="En nombre de semaines. Si moins d’une semaine, laisser 1."
+    bind:value={service.durationWeeks}
+  />
+</FieldSet>
