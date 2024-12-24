59c5e705efdead1b4c7c9ad5e55ef9afc8e905f1
Gérald Gounot <gerald@gounot.eu>
2024-12-24 18:21:32 +0100

    sitemap: index des sitemaps des services

--- front/src/routes/(endpoints)/sitemap.xml/+server.ts
+++ front/src/routes/(endpoints)/sitemap.xml/+server.ts
@@ -3,6 +3,26 @@ import { error } from "@sveltejs/kit";
 import { SITEMAP_PAGE_SIZE } from "$lib/consts";
 import { CANONICAL_URL, ENVIRONMENT } from "$lib/env";
 import { getActiveStructureCount } from "$lib/requests/structures";
+import { getPublishedServiceCount } from "$lib/requests/services";
+
+async function getServiceSitemaps() {
+  const serviceCount = await getPublishedServiceCount();
+
+  if (serviceCount === null) {
+    return "";
+  }
+
+  const pageCount = Math.ceil(serviceCount / SITEMAP_PAGE_SIZE);
+
+  return Array.from({ length: pageCount })
+    .map(
+      (_item, index) =>
+        `<sitemap>
+           <loc>${CANONICAL_URL}/sitemap-services-${index + 1}.xml</loc>
+         </sitemap>`
+    )
+    .join("\n");
+}
 
 async function getStructureSitemaps() {
   const structureCount = await getActiveStructureCount();
@@ -17,7 +37,7 @@ async function getStructureSitemaps() {
     .map(
       (_item, index) =>
         `<sitemap>
-           <loc>${CANONICAL_URL}/sitemap-structures-${index + 1}.xml</loc>
+           <loc>${CANONICAL_URL}/sitemap-services-${index + 1}.xml</loc>
          </sitemap>`
     )
     .join("\n");
@@ -27,6 +47,7 @@ async function getSitemapIndex() {
   return `
   <?xml version="1.0" encoding="UTF-8" ?>
   <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${await getServiceSitemaps()}
     ${await getStructureSitemaps()}
   </sitemapindex>`.trim();
 }
