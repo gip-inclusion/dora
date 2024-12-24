047ce5607c979b443a2f5f6997492d928849913e
Gérald Gounot <gerald@gounot.eu>
2024-12-24 18:32:44 +0100

    Ajout page sitemap-services-[page].xml

---
+++ front/src/routes/(endpoints)/sitemap-services-[page].xml/+server.ts
@@ -0,0 +1,56 @@
+import { error } from "@sveltejs/kit";
+
+import { SITEMAP_PAGE_SIZE } from "$lib/consts";
+import { CANONICAL_URL, ENVIRONMENT } from "$lib/env";
+import { getPublishedServices } from "$lib/requests/services";
+
+import type { RequestHandler } from "./$types";
+
+function toISODate(apiDate) {
+  const date = new Date(apiDate).toISOString();
+  return date.slice(0, date.indexOf("T"));
+}
+
+async function getUrlEntries(page: number) {
+  const publishedServices = await getPublishedServices({
+    pageSize: SITEMAP_PAGE_SIZE,
+    page: page,
+  });
+
+  if (!publishedServices) {
+    return "";
+  }
+
+  return publishedServices
+    .map(
+      (service) =>
+        `<url>
+      <loc>${CANONICAL_URL}/services/${service.slug}</loc>
+      <lastmod>${toISODate(service.modificationDate)}</lastmod>
+      <priority>0.5</priority>
+    </url>`
+    )
+    .join("\n");
+}
+
+async function getUrlSet(page: number) {
+  return `
+  <?xml version="1.0" encoding="UTF-8" ?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${await getUrlEntries(page)}
+  </urlset>`.trim();
+}
+
+export const GET: RequestHandler = async ({ params }) => {
+  if (!["production", "local", "dev"].includes(ENVIRONMENT)) {
+    error(404, "Page Not Found");
+  }
+
+  const urlSet = await getUrlSet(Number(params.page));
+
+  return new Response(urlSet, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+};
