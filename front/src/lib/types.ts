8a092fae809018eed0b1d670d714bb470609cea3
Gérald Gounot <gerald@gounot.eu>
2024-12-30 18:52:03 +0100

    Ajout du type front UpdateFrequency

--- front/src/lib/types.ts
+++ front/src/lib/types.ts
@@ -307,6 +307,13 @@ export type BeneficiaryAccessModes =
   | "telephoner"
   | "autre";
 
+export type UpdateFrequency =
+  | "tous-les-mois"
+  | "tous-les-3-mois"
+  | "tous-les-6-mois"
+  | "tous-les-12-mois"
+  | "jamais";
+
 export interface FundingLabel {
   value: string;
   label: string;
@@ -474,6 +481,7 @@ export interface Service {
   subcategoriesDisplay: string[];
   suspensionDate: string;
   useInclusionNumeriqueScheme: boolean;
+  updateFrequency: UpdateFrequency;
   updateStatus: ServiceUpdateStatus;
 }
 
