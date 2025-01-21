62e41af65e26637826f57f0308bef6ea052acac5
Gérald Gounot <gerald@gounot.eu>
2025-01-21 20:40:21 +0100

    make_di_service_data(): ajout d'un champ score_qualite à 0.5 par défaut

--- back/dora/data_inclusion/test_utils.py
+++ back/dora/data_inclusion/test_utils.py
@@ -47,6 +47,7 @@ def make_di_service_data(**kwargs) -> dict:
             "modes_orientation_accompagnateur_autres": "Mêmes modalités que pour les bénéficiaires",
             "modes_orientation_beneficiaire": ["telephoner", "autre"],
             "modes_orientation_beneficiaire_autres": "Contacter conseiller(e) Pôle Emploi",
+            "score_qualite": 0.5,
             "zone_diffusion_type": None,
             "zone_diffusion_code": None,
             "zone_diffusion_nom": "foo",
