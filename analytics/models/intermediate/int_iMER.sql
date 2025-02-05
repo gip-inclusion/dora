ba3e61a7322aa1c8216cb77aa74b4c197fd2de80
laurinehu <laurinehu@gmail.com>
2025-02-05 20:01:57 +0100

    feat: ajout d'une table iMER

---
+++ analytics/models/intermediate/int_iMER.sql
@@ -0,0 +1,22 @@
+SELECT
+    'orientation'             as kind,
+    orientation_creation_date as date,
+    orientation_prescriber_id as user_id,
+    service_id,
+    CASE 
+        WHEN orientation_di_service_id IS NOT NULL THEN TRUE ELSE FALSE 
+    END                       as di_service,
+    CASE
+        WHEN user_main_activity = 'accompagnateur' or user_main_activity = 'accompagnateur_offreur' THEN TRUE ELSE FALSE
+    END                       as is_accompagnateur
+FROM {{ ref('int_orientation_user_service') }}
+UNION ALL
+SELECT
+    'mobilisation'            as kind,
+    event_date                as date,
+    user_id                   as user_id,
+    event_is_di               as di_service,
+    CASE
+        WHEN user_main_activity = 'accompagnateur' or user_main_activity = 'accompagnateur_offreur' THEN TRUE ELSE FALSE
+    END                       as is_accompagnateur
+FROM {{ ref('int_mobilisationevent_user') }}
