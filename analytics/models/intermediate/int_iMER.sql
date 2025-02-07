6541d1688dc23c08facb378394a0f8b45d3d61f0
laurinehu <laurinehu@gmail.com>
2025-02-07 10:50:46 +0100

    minor naming modif

--- analytics/models/intermediate/int_iMER.sql
+++ analytics/models/intermediate/int_iMER.sql
@@ -5,7 +5,7 @@ SELECT
     service_id,
     CASE 
         WHEN orientation_di_service_id IS NOT NULL THEN TRUE ELSE FALSE 
-    END                       as di_service,
+    END                       as is_di_service,
     CASE
         WHEN user_main_activity = 'accompagnateur' or user_main_activity = 'accompagnateur_offreur' THEN TRUE ELSE FALSE
     END                       as is_prescriber
@@ -15,7 +15,7 @@ SELECT
     'mobilisation'            as kind,
     event_date                as date,
     user_id                   as user_id,
-    event_is_di               as di_service,
+    event_is_di               as is_di_service,
     CASE
         WHEN user_main_activity = 'accompagnateur' or user_main_activity = 'accompagnateur_offreur' THEN TRUE ELSE FALSE
     END                       as is_prescriber
