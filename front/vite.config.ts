24f030144408dc7c087bdd863200ed47f186f5cf
Gérald Gounot <gerald@gounot.eu>
2025-02-12 15:16:53 +0100

    Installation de @tailwindcss/vite

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig, loadEnv } from "vite";
 import { sentrySvelteKit } from "@sentry/sveltekit";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
   // Charge les variables d'environnement des fichiers .env
@@ -13,6 +14,7 @@ export default defineConfig(({ mode }) => {
         },
       }),
       sveltekit(),
+      tailwindcss(),
     ],
 
     build: {
