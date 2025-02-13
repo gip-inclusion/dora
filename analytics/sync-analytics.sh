b232680282ce3cd3fa73133a3758844bfee7b408
Gérald Gounot <gerald@gounot.eu>
2025-02-13 14:22:40 +0100

    feat(analytics): utilisation d'un fichier .env pour le chargement des variables d'environnement analytics (#241)

--- analytics/sync-analytics.sh
+++ analytics/sync-analytics.sh
@@ -4,6 +4,30 @@
 # you only have to provide the correct DATABASE_URL and DORA_DATABASE_URL
 # with an enabled proxy to the DORA database.
 
+# Chargement des variables d'environnement à partir du fichier .env s'il existe
+if [ -f .env ]; then
+    echo "Chargement des variables d'environnement à partir du fichier .env"
+    source .env
+fi
+
+# Vérification si les variables d'environnement requises sont définies
+required_vars=("DORA_DATABASE_URL" "DATABASE_URL")
+missing_vars=()
+
+for var in "${required_vars[@]}"; do
+    if [ -z "${!var}" ]; then
+        missing_vars+=("$var")
+    fi
+done
+
+if [ ${#missing_vars[@]} -ne 0 ]; then
+    echo "Erreur : variables d'environnement requises manquantes :"
+    for var in "${missing_vars[@]}"; do
+        echo "- $var"
+    done
+    exit 1
+fi
+
 if command -v dbclient-fetcher &> /dev/null; then
     dbclient-fetcher pgsql 15
 fi
