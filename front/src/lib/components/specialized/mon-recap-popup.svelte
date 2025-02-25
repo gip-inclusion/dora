0907a9b3a7eefc9146c1820cd1126ff82212af08
Jérémy Buget <jbuget@gmail.com>
2025-02-25 12:46:47 +0100

    fix(linting): Correction de problèmes de formattage du code

--- front/src/lib/components/specialized/mon-recap-popup.svelte
+++ front/src/lib/components/specialized/mon-recap-popup.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
-	import dayjs from "dayjs";
+  import dayjs from "dayjs";
 
   import { browser } from "$app/environment";
   import TallyPopup from "$lib/components/specialized/tally-popup.svelte";
   import { TallyFormId, MON_RECAP_DEPARTMENTS } from "$lib/consts";
   import { userInfo } from "$lib/utils/auth";
 
-let monRecapTallyFormCompletedKey = `tallyForm-${TallyFormId.NOTEBOOK_ORDER_FORM_ID}-completed`;
+  const monRecapTallyFormCompletedKey = `tallyForm-${TallyFormId.NOTEBOOK_ORDER_FORM_ID}-completed`;
 
-let shouldDisplayMonRecapForm = 
-  $userInfo &&
-  ($userInfo.mainActivity === "accompagnateur" || $userInfo.mainActivity === "accompagnateur_offreur") &&
-  $userInfo.structures.some(struct => MON_RECAP_DEPARTMENTS.includes(struct.department)) &&
-  !localStorage.getItem(monRecapTallyFormCompletedKey);
-
+  let shouldDisplayMonRecapForm =
+    $userInfo &&
+    ($userInfo.mainActivity === "accompagnateur" ||
+      $userInfo.mainActivity === "accompagnateur_offreur") &&
+    $userInfo.structures.some((struct) =>
+      MON_RECAP_DEPARTMENTS.includes(struct.department)
+    ) &&
+    !localStorage.getItem(monRecapTallyFormCompletedKey);
 </script>
 
 {#if browser && shouldDisplayMonRecapForm}
