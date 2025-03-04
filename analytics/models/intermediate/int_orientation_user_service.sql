660ec608445cd68441bb00576781364f17732010
laurinehu <laurinehu@gmail.com>
2025-03-04 13:21:21 +0100

    add monthly user stats for services views, orientations, mobilisations and iMER

--- analytics/models/intermediate/int_orientation_user_service.sql
+++ analytics/models/intermediate/int_orientation_user_service.sql
@@ -1,23 +1,9 @@
-WITH orientations AS (
-    SELECT * FROM {{ ref('stg_orientation') }}
-),
-
-users AS (
-    SELECT * FROM {{ ref('stg_user') }}
-),
-
-services AS (
-    SELECT * FROM {{ ref('int_service_structure') }}
-),
-
-final AS (
-    SELECT
-        {{ dbt_utils.star(relation_alias='orientations', from=ref('stg_orientation'), prefix='orientation_') }},
-        {{ dbt_utils.star(relation_alias='users', from=ref('stg_user'), prefix='user_') }},
-        {{ dbt_utils.star(relation_alias='services', from=ref('int_service_structure')) }}
-    FROM orientations
-    INNER JOIN users ON orientations.prescriber_id = users.id
-    INNER JOIN services ON orientations.service_id = services.service_id
-)
-
-SELECT * FROM final
+SELECT
+    {{ dbt_utils.star(relation_alias='orientations', from=ref('stg_orientation'), prefix='orientation_') }},
+    {{ dbt_utils.star(relation_alias='users', from=ref('stg_user'), prefix='user_') }},
+    {{ dbt_utils.star(relation_alias='services', from=ref('int_service_structure')) }}
+FROM {{ ref('stg_orientation') }} AS orientations
+INNER JOIN {{ ref('stg_user') }} AS users
+    ON orientations.prescriber_id = users.id
+INNER JOIN {{ ref('int_service_structure') }} AS services
+    ON orientations.service_id = services.service_id
