8b0caf971387e408e69832bd22540e9e69439482
laurinehu <laurine.huber@inclusion.gouv.fr>
2025-03-04 22:01:14 +0100

    feat(data): tables de suivi mensuel des actions utilisateurs (#273)
    Co-authored-by: laurinehu <laurinehu@gmail.com>

--- analytics/models/intermediate/int_iMER.sql
+++ analytics/models/intermediate/int_iMER.sql
@@ -1,19 +1,32 @@
 SELECT
     'orientation'                                                                       AS kind,
+    CAST(orientation_id AS text)                                                        AS event_id,
     orientation_creation_date                                                           AS date,
     orientation_prescriber_id                                                           AS user_id,
+    user_main_activity                                                                  AS user_kind,
+    user_is_manager                                                                     AS is_manager,
+    structure_typology,
+    structure_department,
     COALESCE(orientation_di_service_id IS NOT NULL, FALSE)                              AS is_di_service,
     COALESCE(user_main_activity IN ('accompagnateur', 'accompagnateur_offreur'), FALSE) AS is_prescriber,
     NULL                                                                                AS generates_orientation
 FROM {{ ref('int_orientation_user_service') }}
 UNION ALL
 SELECT
-    'mobilisation'                                                                      AS kind,
-    event_date                                                                          AS date,
+    'mobilisation'                                                                        AS kind,
+    m.event_id,
+    event_date                                                                            AS date,
     m.user_id,
-    event_is_di                                                                         AS is_di_service,
-    COALESCE(user_main_activity IN ('accompagnateur', 'accompagnateur_offreur'), FALSE) AS is_prescriber,
-    o_m.delay IS NOT NULL                                                               AS generates_orientation
+    m.event_user_kind                                                                     AS user_kind,
+    m.event_is_manager                                                                    AS is_manager,
+    struct_members.structure_typology,
+    struct_members.structure_department,
+    event_is_di                                                                           AS is_di_service,
+    COALESCE(m.user_main_activity IN ('accompagnateur', 'accompagnateur_offreur'), FALSE) AS is_prescriber,
+    o_m.delay IS NOT NULL                                                                 AS generates_orientation
 FROM {{ ref('int_mobilisationevent_user') }} as m
+LEFT JOIN
+    {{ ref('int_structure_members') }} AS struct_members
+    ON m.user_id = struct_members.user_id AND m.event_structure_id = CAST(struct_members.structure_id AS text)
 LEFT JOIN {{ ref('int_orientations_following_mobilisation') }} as o_m
     ON o_m.mobilisation_id = m.event_id
