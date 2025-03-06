030b7a92df9682d26cb96d3b9ad56395901edfbf
Frédéric Vergez <fred@ikarius.com>
2025-03-06 13:37:21 +0100

    fix: prettier sur fichier frontend
    Pas automatique ?

--- front/src/lib/consts.ts
+++ front/src/lib/consts.ts
@@ -1,6 +1,5 @@
 export const SIREN_FRANCE_TRAVAIL = "130005481";
 
-// eslint-disable-next-line no-shadow
 export const enum TallyFormId {
   SERVICE_CREATION_FORM_ID = "mRGdpK",
 }
--- front/src/lib/types.ts
+++ front/src/lib/types.ts
@@ -485,7 +485,7 @@ export interface Service {
   updateFrequency: UpdateFrequency | null;
   updateFrequencyDisplay: string | null;
   updateStatus: ServiceUpdateStatus;
-  isOrientableFtService:boolean;
+  isOrientableFtService: boolean;
 }
 
 export interface ShortService {
