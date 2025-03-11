61bfe45a8406d8fd37d9e92dd9154d94deec9551
Gérald Gounot <gerald@gounot.eu>
2025-03-11 16:14:44 +0100

    Suppression du périmètre

--- front/src/routes/(modeles-services)/services/[slug]/service-header.svelte
+++ front/src/routes/(modeles-services)/services/[slug]/service-header.svelte
@@ -23,11 +23,11 @@
     {service.name}
   </h1>
   <div
-    class="mb-s32 mt-s16 text-f14 print:mb-s8 print:text-france-blue text-gray-text flex flex-col md:flex-row md:items-center"
+    class="mt-s16 text-f14 print:mb-s8 print:text-france-blue text-gray-text flex flex-col md:flex-row md:items-center"
   >
-    <div>
-      <strong
-        >{#if !isDI}
+    <span
+      ><strong>
+        {#if !isDI}
           <a href="/structures/{service.structureInfo.slug}" class="underline"
             >{capitalize(service.structureInfo.name)}</a
           >
@@ -36,14 +36,6 @@
         {/if}
       </strong>
       à {service.structureInfo.city} ({service.structureInfo.department})
-    </div>
-  </div>
-
-  <div
-    class="text-f18 print:text-france-blue text-gray-text md:flex-row md:items-center"
-  >
-    Périmètre : <strong
-      >{service.diffusionZoneDetailsDisplay || "Non renseigné"}</strong
-    >
+    </span>
   </div>
 </div>
