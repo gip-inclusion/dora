44ec4fd69ba116377b5c4fc2ed760710ab3c8534
Gérald Gounot <gerald@gounot.eu>
2025-03-12 10:35:01 +0100

    Ajout du composant ServiceActionButton

---
+++ front/src/routes/(modeles-services)/services/[slug]/service-action-button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let ariaLabel: string;
+</script>
+
+<button
+  class="border-gray-03 text-gray-text-alt py-s12 px-s20 hover:text-magenta-cta hover:border-magenta-cta border-1"
+  {ariaLabel}
+  on:click
+>
+  <slot />
+</button>
