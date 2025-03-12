672f62780f062746de18ad2de7cc3ff3b841b6c0
Gérald Gounot <gerald@gounot.eu>
2025-03-12 16:15:48 +0100

    ServiceHeader : réorganisation du nom de la structure et du périmètre d'éligibilité

--- front/src/routes/(modeles-services)/services/[slug]/service-header.svelte
+++ front/src/routes/(modeles-services)/services/[slug]/service-header.svelte
@@ -1,31 +1,35 @@
 <script lang="ts">
+  import HomeSmileLineBuildings from "svelte-remix/HomeSmileLineBuildings.svelte";
   import Breadcrumb from "$lib/components/display/breadcrumb.svelte";
   import type { Service } from "$lib/types";
   import { capitalize } from "$lib/utils/misc";
 
   export let service: Service;
   export let isDI: false;
+
+  $: diffusionZoneDetails = service.diffusionZoneDetails
+    ? `(${service.diffusionZoneDetails})`
+    : "";
+  $: diffusionZone = service.diffusionZoneDetailsDisplay
+    ? [service.diffusionZoneDetailsDisplay, diffusionZoneDetails]
+        .filter(Boolean)
+        .join(" ")
+    : "Non renseigné";
 </script>
 
-<div
-  id="service-header"
-  class="gap-s16 print:gap-s0 relative lg:flex-row-reverse lg:justify-between"
->
-  <div class="mb-s24 print:mb-s0">
+<div class="gap-s24 text-gray-text flex flex-col">
+  <div>
     <Breadcrumb
       {service}
       structure={service.structureInfo}
       currentLocation="service"
     />
   </div>
-  <h1 class="mb-s0 mr-s12 text-magenta-dark text-f38 leading-[3rem]">
-    {service.name}
-  </h1>
-  <div
-    class="mt-s16 text-f14 print:mb-s8 print:text-france-blue text-gray-text flex flex-col md:flex-row md:items-center"
-  >
-    <span
-      ><strong>
+
+  <div class="gap-s16 flex flex-col">
+    <div class="gap-s6 text-f14 flex items-center">
+      <HomeSmileLineBuildings size="16" />
+      <strong>
         {#if !isDI}
           <a href="/structures/{service.structureInfo.slug}" class="underline"
             >{capitalize(service.structureInfo.name)}</a
@@ -34,7 +38,12 @@
           {capitalize(service.structureInfo.name)}
         {/if}
       </strong>
-      à {service.structureInfo.city} ({service.structureInfo.department})
-    </span>
+    </div>
+    <h1 class="mb-s0 mr-s12 text-magenta-dark text-f38 leading-s48">
+      {service.name}
+    </h1>
+    <div class="text-f14">
+      Périmètre d’éligibilité&#8239;: {diffusionZone}
+    </div>
   </div>
 </div>
