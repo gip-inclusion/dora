657d366d5d8bca328fee997c55068dcb4bce54d2
Gérald Gounot <gerald@gounot.eu>
2025-03-12 17:55:36 +0100

    Redesign de ModelHeader

--- front/src/routes/(modeles-services)/modeles/[slug]/model-header.svelte
+++ front/src/routes/(modeles-services)/modeles/[slug]/model-header.svelte
@@ -1,39 +1,32 @@
 <script lang="ts">
+  import HomeSmileLineBuildings from "svelte-remix/HomeSmileLineBuildings.svelte";
   import Breadcrumb from "$lib/components/display/breadcrumb.svelte";
-  import { copyIcon2Fill } from "$lib/icons";
   import { capitalize } from "$lib/utils/misc";
+  import type { Model } from "$lib/types";
 
-  export let model;
+  export let model: Model;
 </script>
 
-<div
-  id="service-header"
-  class="gap-s16 relative lg:flex-row-reverse lg:justify-between"
->
-  <div class="mb-s48 print:mb-s0">
-    <Breadcrumb
-      {model}
-      structure={model.structureInfo}
-      currentLocation="model"
-    />
+<div class="text-gray-text">
+  <Breadcrumb {model} structure={model.structureInfo} currentLocation="model" />
+
+  <div class="gap-s6 text-f14 mt-s48 flex items-center">
+    <HomeSmileLineBuildings size="16" />
+    <strong>
+      <a href="/structures/{model.structureInfo.slug}" class="underline"
+        >{capitalize(model.structureInfo.name)}</a
+      >
+    </strong>
   </div>
+
+  <h1 class="text-magenta-dark text-f38 leading-s48 mt-s16 mb-s24">
+    {model.name}
+  </h1>
 
   <div
-    class="mb-s28 bg-france-blue px-s12 py-s8 text-f14 inline-flex items-center rounded-lg text-white"
+    class=" bg-blue-information-dark px-s8 py-s2 text-f12 inline-flex items-center rounded-sm font-bold text-white uppercase"
   >
-    <span class="mr-s8 h-s16 w-s16 inline-block fill-current text-white">
-      {@html copyIcon2Fill}
-    </span>
     Modèle&nbsp;•&nbsp;{model.numServices}
     {model.numServices > 1 ? "services synchronisés" : "service synchronisé"}
-  </div>
-
-  <div>
-    <h1 class="mb-s12 mr-s12 text-france-blue">
-      {model.name}
-    </h1>
-    <p class="text-f23 text-france-blue font-bold">
-      {capitalize(model.structureInfo.name)}
-    </p>
   </div>
 </div>
