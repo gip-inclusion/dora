5b2c68021f8256dfbf94e3de701368cad5a8d006
Gérald Gounot <gerald@gounot.eu>
2025-03-14 18:33:24 +0100

    Ajout de la section Autres informations

--- front/src/routes/(modeles-services)/_common/display/service-presentation.svelte
+++ front/src/routes/(modeles-services)/_common/display/service-presentation.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
-  import { page } from "$app/stores";
-  import ServiceKeyInformations from "$lib/components/specialized/services/display/service-key-informations.svelte";
-  import ServiceSteps from "$lib/components/specialized/services/display/service-steps.svelte";
   import ServiceDocuments from "$lib/components/specialized/services/display/service-documents.svelte";
+  import ServiceKeyInformations from "$lib/components/specialized/services/display/service-key-informations.svelte";
+  import ServiceOtherInformations from "$lib/components/specialized/services/display/service-other-informations.svelte";
+  import ServiceSteps from "$lib/components/specialized/services/display/service-steps.svelte";
   import type { Model, Service, ServicesOptions } from "$lib/types";
-  import Notice from "$lib/components/display/notice.svelte";
 
   import ServiceDescription from "../service-description.svelte";
+
   export let service: Service | Model;
   export let servicesOptions: ServicesOptions;
-  export let isDI = false;
 </script>
 
 <div class="gap-s36 flex flex-col">
@@ -26,22 +25,6 @@
   <ServiceSteps {service} />
 
   <ServiceDocuments {service} />
-</div>
-<div class="mb-s40 print:mb-s24" />
 
-{#if isDI}
-  <Notice
-    title="Ce service provient de {service.source}, via data·inclusion"
-    type="info"
-  >
-    <p class="text-f14">
-      Ce service est automatiquement récupéré depuis le référentiel commun
-      data·inclusion, auquel participe DORA. N’hésitez pas à <a
-        class="underline"
-        href="https://tally.so/r/nrBNqv?url={encodeURIComponent(
-          $page.url.origin + $page.url.pathname
-        )}">nous faire part de vos retours</a
-      > si vous remarquez des erreurs.
-    </p>
-  </Notice>
-{/if}
+  <ServiceOtherInformations {service} {servicesOptions} />
+</div>
