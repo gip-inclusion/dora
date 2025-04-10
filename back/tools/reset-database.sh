7285f6fa694da05a4e91312e496c381545f3db45
Gérald Gounot <gerald@gounot.eu>
2025-04-10 12:10:54 +0200

    reset-database.sh : mise à jour des emojis

--- back/tools/reset-database.sh
+++ back/tools/reset-database.sh
@@ -14,35 +14,35 @@ REQUIRED_ENV_VARS=("ENVIRONMENT" "DORA_BACK_STAGING_REGION" "DORA_BACK_STAGING_A
 
 echo -e "${CYAN}🔍 Vérification de l'environnement…${NC}"
 if [ "${ENVIRONMENT}" != "review" ];then
-  echo -e "${RED}La réinitialisation de la base de données ne peut se faire que sur une review app.${NC}"
+  echo -e "${RED}❌ La réinitialisation de la base de données ne peut se faire que sur une review app.${NC}"
   exit 0;
 fi
 echo ""
 
-echo -e "${CYAN}🛠️  Vérification des variables d'environnement…${NC}"
+echo -e "${CYAN}🔧 Vérification des variables d'environnement…${NC}"
 for var in "${REQUIRED_ENV_VARS[@]}"; do
   if [ -z "${!var}" ]; then
-    echo -e "${RED}La variable d'environnement $var n'est pas définie.${NC}"
+    echo -e "${RED}❌ La variable d'environnement $var n'est pas définie.${NC}"
     exit 1
   fi
 done
 echo ""
 
 if command -v dbclient-fetcher &>/dev/null; then
-  echo -e "${CYAN}🐘  Installation des outils PostgreSQL…${NC}"
+  echo -e "${CYAN}🐘 Installation des outils PostgreSQL…${NC}"
   dbclient-fetcher pgsql
   echo ""
 elif ! command -v pg_restore &> /dev/null; then
-  echo -e "${RED}⚠️  pg_restore n'est pas installé. Veuillez l'installer avant d'exécuter ce script.${NC}"
+  echo -e "${RED}❌ pg_restore n'est pas installé. Veuillez l'installer avant d'exécuter ce script.${NC}"
   exit 1
 fi
 
 if command -v install-scalingo-cli &>/dev/null; then
-  echo -e "${CYAN}⬇️  Installation de Scalingo CLI…${NC}"
+  echo -e "${CYAN}⬇️ Installation de Scalingo CLI…${NC}"
   install-scalingo-cli
   echo ""
 elif ! command -v scalingo &> /dev/null; then
-  echo -e "${RED}⚠️  Scalingo CLI n'est pas installé. Veuillez l'installer avant d'exécuter ce script.${NC}"
+  echo -e "${RED}❌ Scalingo CLI n'est pas installé. Veuillez l'installer avant d'exécuter ce script.${NC}"
   exit 1
 fi
 
@@ -50,8 +50,8 @@ echo -e "${CYAN}🔐 Vérification de la connexion à Scalingo…${NC}"
 echo ""
 if ! scalingo whoami; then
   echo -e "${CYAN}🔑 Connexion à Scalingo…${NC}"
-scalingo login --api-token "${SCALINGO_API_TOKEN}"
-echo ""
+  scalingo login --api-token "${SCALINGO_API_TOKEN}"
+  echo ""
 fi
 
 echo -e "${CYAN}📂 Création d'un répertoire temporaire…${NC}"
@@ -60,19 +60,19 @@ echo -e "${YELLOW}→ Utilisation du répertoire temporaire : $temp_dir${NC}"
 cd "$temp_dir"
 echo ""
 
-echo -e "${CYAN}📥  Récupération de la dernière sauvegarde de staging…${NC}"
+echo -e "${CYAN}📥 Récupération de la dernière sauvegarde de staging…${NC}"
 scalingo --region "${DORA_BACK_STAGING_REGION}" --app "${DORA_BACK_STAGING_APP}" --addon "${DORA_BACK_STAGING_ADDON}" backups-download
 archive_filename=$(ls *.tar.gz)
 echo -e "${YELLOW}→ Fichier : $archive_filename${NC}"
 echo ""
 
-echo -e "${CYAN}📂  Décompression de l'archive…${NC}"
+echo -e "${CYAN}📦 Décompression de l'archive…${NC}"
 tar -xzvf "$archive_filename"
 decompressed_filename="${archive_filename%.tar.gz}.pgsql"
 echo -e "${YELLOW}→ Fichier : $decompressed_filename${NC}"
 echo ""
 
-echo -e "${CYAN}🔄  Restauration des données…${NC}"
+echo -e "${CYAN}🔄 Restauration des données…${NC}"
 pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --dbname "$DATABASE_URL" "$decompressed_filename"
 echo ""
 
@@ -81,4 +81,4 @@ cd -
 rm -rf "$temp_dir"
 echo ""
 
-echo -e "${GREEN}✅  Restauration des données terminée !${NC}"
+echo -e "${GREEN}✨ Restauration des données terminée !${NC}"
