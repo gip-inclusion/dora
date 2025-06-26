fc3599d3eec75e991c96b9380280a21e68f89f7a
Frédéric Vergez <fred@ikarius.com>
2025-06-26 11:48:38 +0200

    fix: adaptation des tests

--- back/dora/auth_links/tests/test_views.py
+++ back/dora/auth_links/tests/test_views.py
@@ -1,5 +1,3 @@
-from uuid import uuid4
-
 import sesame.utils
 from django.conf import settings
 from django.shortcuts import reverse
@@ -10,29 +8,6 @@ from dora.core.test_utils import make_user
 
 # note : inutile de tester la durée de validité et l'usage unique,
 # c'est de la response de `django-sesame`
-
-
-def test_send_link(client):
-    # aucun lien n'est envoyé si l'utilisateur n'a pas un compte IC
-    user_no_ic = make_user()
-    response = client.post(
-        reverse("send_link"),
-        data={"email": user_no_ic.email},
-    )
-    assert response.status_code == 404, (
-        "Une response 404 est attendue (utilisateur sans IC)"
-    )
-
-    # on vérifie qu'un lien est envoyé si l'utilisateur à un compte IC
-    user_ic = make_user(ic_id=uuid4(), is_active=True)
-
-    response = client.post(
-        reverse("send_link"),
-        data={"email": user_ic.email},
-    )
-    assert response.status_code == 204, (
-        "Une response 204 est attendue (utilisateur avec IC trouvé)"
-    )
 
 
 def test_authenticate_with_link(client):
@@ -46,7 +21,7 @@ def test_authenticate_with_link(client):
     )
 
     # Redirection vers l'identification du frontend si le code fourni est valide
-    user = make_user(ic_id=uuid4(), is_active=True)
+    user = make_user(is_active=True)
 
     response = client.get(
         reverse(
