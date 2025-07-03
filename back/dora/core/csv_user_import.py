3a0ae61037856edd764d2913bcd16e318b49becf
David Martin Cohen <davidmartincohen@gmail.com>
2025-07-03 17:51:37 +0200

    ajouter des tests

--- back/dora/core/csv_user_import.py
+++ back/dora/core/csv_user_import.py
@@ -16,22 +16,17 @@ from dora.users.models import User
 
 
 class ImportUserHelper:
-    def __init__(self):
-        self.is_france_travail = False
-
     def import_france_travail_users(
         self,
         reader,
         wet_run=False,
         make_users_admin=False,
     ):
-        if wet_run:
-            print("PRODUCTION RUN")
-        else:
-            print("DRY RUN")
-
         errors = []
-        for i, row in enumerate(reader):
+        users_to_import = []
+        self._display_run_type(wet_run)
+
+        for index, row in enumerate(reader):
             try:
                 safir = row["safir"]
                 structure = self._structure_by_safir(safir)
@@ -40,60 +35,73 @@ class ImportUserHelper:
                 )
                 first_name = row.get("prenom")
                 last_name = row.get("nom")
-            except Exception as ex:
-                line_number = i + 1
-                print(f"Erreur de traitement Ligne {line_number}: {ex}")
-                errors.append(line_number)
-            else:
-                print(f"{i}. Import pour l'agence avec le code SAFIR : {safir}")
 
-                if not errors and wet_run:
-                    print(
-                        self.invite_user(
-                            structure,
-                            email,
-                            first_name,
-                            last_name,
-                            is_france_travail=True,
-                            admin=make_users_admin,
-                        )
+                users_to_import.append(
+                    {
+                        "structure": structure,
+                        "email": email,
+                        "first_name": first_name,
+                        "last_name": last_name,
+                    }
+                )
+            except Exception as ex:
+                errors.append(f"[{index + 1}]: {ex}")
+
+        if errors:
+            print("Il y a des erreurs qui empêchent l'import :")
+            print(("\n".join(errors)))
+
+        else:
+            for index, user in enumerate(users_to_import):
+                print(
+                    f"{index}. Import {user['first_name']} {user['last_name']} pour l'agence avec le code SAFIR : {safir}"
+                )
+                if wet_run:
+                    self.invite_user(
+                        user,
+                        is_france_travail=True,
+                        admin=make_users_admin,
                     )
 
     def import_users(self, reader, wet_run=False, make_users_admin=False):
         errors = []
+        users_to_import = []
+        self._display_run_type(wet_run)
 
-        if wet_run:
-            print("PRODUCTION RUN")
-        else:
-            print("DRY RUN")
-
-        for i, row in enumerate(reader):
+        for index, row in enumerate(reader):
             try:
                 structure_siret = row.get("structure_siret")
                 structure = self._structure_by_siret(structure_siret)
                 email = row.get("email")
                 first_name = row.get("prenom")
                 last_name = row.get("nom")
-            except Exception as ex:
-                line_number = i + 1
-                print(f"Erreur de traitement Ligne {line_number}: {ex}")
-                errors.append(line_number)
 
-            else:
-                print(
-                    f"{i}. Import pour la structure avec le SIRET : {structure_siret}"
+                users_to_import.append(
+                    {
+                        "structure": structure,
+                        "email": email,
+                        "first_name": first_name,
+                        "last_name": last_name,
+                    }
                 )
 
-                if not errors and wet_run:
-                    print(
-                        self.invite_user(
-                            structure,
-                            email,
-                            first_name,
-                            last_name,
-                            is_france_travail=False,
-                            admin=make_users_admin,
-                        )
+            except Exception as ex:
+                errors.append(f"[{index + 1}]: {ex}")
+
+        if errors:
+            print("Il y a des erreurs qui empêchent l'import :")
+            print(("\n".join(errors)))
+
+        else:
+            for index, user in enumerate(users_to_import):
+                print(
+                    f"{index}. Import {user['first_name']} {user['last_name']} pour la structure avec le SIRET : {structure_siret}"
+                )
+                if wet_run:
+                    self.invite_user(
+                        user,
+                        is_france_travail=False,
+                        admin=make_users_admin,
                     )
 
     @staticmethod
@@ -114,33 +122,17 @@ class ImportUserHelper:
             " ", "-"
         )
 
-    @staticmethod
-    def _structure_by_siret(siret: str) -> Structure:
+    def _structure_by_siret(self, siret: str) -> Structure:
         try:
             return Structure.objects.get(siret=siret)
-        except Structure.DoesNotExist as ex:
-            raise Exception(
-                f"La structure avec le SIRET {siret} n'existe pas en base"
-            ) from ex
+        except Structure.DoesNotExist:
+            raise Exception(f"La structure avec le SIRET {siret} n'existe pas en base")
 
-    @staticmethod
-    def _structure_by_safir(safir: str) -> Structure | None:
+    def _structure_by_safir(self, safir: str) -> Structure | None:
         try:
             return Structure.objects.get(code_safir_pe=safir)
-        except Structure.DoesNotExist as ex:
-            raise Exception(
-                f"L'agence FT avec le code {safir} n'existe pas en base"
-            ) from ex
-
-    def _get_structure(self, structure_id: str) -> Structure | None:
-        filter_key = "code_safir_pe" if self.is_france_travail else "id"
-        filter = {[filter_key]: structure_id}
-        try:
-            return Structure.objects.get(**filter)
         except Structure.DoesNotExist:
-            raise Exception(
-                f"La structure avec {'le code safir' if self.is_france_travail else "l'id"} : {structure_id} n'existe pas en base"
-            )
+            raise Exception(f"L'agence FT avec le code {safir} n'existe pas en base")
 
     @staticmethod
     def _maybe_upgrade_as_admin(member, admin: bool) -> str:
@@ -150,9 +142,12 @@ class ImportUserHelper:
             return " (mais désormais en tant qu'admin)"
         return ""
 
-    def invite_user(
-        self, structure, email, first_name, last_name, is_france_travail, admin=True
-    ) -> str:
+    def invite_user(self, user, is_france_travail, admin=True) -> None:
+        email = user["email"]
+        first_name = user["first_name"]
+        last_name = user["last_name"]
+        structure = user["structure"]
+
         try:
             user = User.objects.get_by_email(email)
         except User.DoesNotExist:
@@ -193,4 +188,11 @@ class ImportUserHelper:
                 else:
                     send_invitation_email(member, inviter_name)
 
-        return result
+        print(result)
+
+    @staticmethod
+    def _display_run_type(wet_run):
+        if wet_run:
+            print("PRODUCTION RUN")
+        else:
+            print("DRY RUN")
