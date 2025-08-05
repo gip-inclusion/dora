99081abe60ffee472f025a61de4631012954afe8
Gérald Gounot <gerald@gounot.eu>
2025-08-05 17:13:06 +0200

    fix: correction du formatage des numéros de téléphone avec préfixe (#489)

--- front/src/tests/utils/misc/formatPhoneNumber.test.ts
+++ front/src/tests/utils/misc/formatPhoneNumber.test.ts
@@ -25,4 +25,32 @@ describe("formatPhoneNumber", () => {
   test("ignore les caractères non numériques", () => {
     expect(formatPhoneNumber("06a12b34c56d78")).toBe("06 12 34 56 78");
   });
+
+  test("convertit +33 en 0 pour les numéros français", () => {
+    expect(formatPhoneNumber("+33 1 23 45 67 89")).toBe("01 23 45 67 89");
+  });
+
+  test("convertit +33 en 0 sans espaces", () => {
+    expect(formatPhoneNumber("+33123456789")).toBe("01 23 45 67 89");
+  });
+
+  test("convertit +33 en 0 pour un numéro mobile", () => {
+    expect(formatPhoneNumber("+33 6 12 34 56 78")).toBe("06 12 34 56 78");
+  });
+
+  test("garde le + pour les autres préfixes internationaux", () => {
+    expect(formatPhoneNumber("+1 555 123 4567")).toBe("+155 51 23 45 67");
+  });
+
+  test("garde le + pour les autres préfixes sans espaces", () => {
+    expect(formatPhoneNumber("+15551234567")).toBe("+155 51 23 45 67");
+  });
+
+  test("garde le + pour les préfixes européens", () => {
+    expect(formatPhoneNumber("+44 20 7946 0958")).toBe("+442 07 94 60 95 8");
+  });
+
+  test("gère un numéro avec +33 et caractères non numériques", () => {
+    expect(formatPhoneNumber("+33a1b2c3d4e5f6g7h8i9")).toBe("01 23 45 67 89");
+  });
 });
