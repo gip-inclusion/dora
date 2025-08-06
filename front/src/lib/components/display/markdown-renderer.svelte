341eacdfb0b5ad3b6340bf253de643381127f1e7
Gérald Gounot <gerald@gounot.eu>
2025-08-06 20:35:18 +0200

    MarkdownRender: désactivation de la régle svelte/no-at-html-tags

--- front/src/lib/components/display/markdown-renderer.svelte
+++ front/src/lib/components/display/markdown-renderer.svelte
@@ -1,4 +1,10 @@
 <script lang="ts">
+  /* eslint-disable svelte/no-at-html-tags */
+  /*
+   * Pour des raisons de sécurité, on centralise l'affichage du rendu Markdown
+   * en utilisant @html dans ce fichier.
+   * On utilise markdownToHTML() qui fait appel à insane() pour sécuriser le HTML.
+   */
   import { markdownToHTML } from "$lib/utils/misc";
 
   interface Props {
