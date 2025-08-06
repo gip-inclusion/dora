2a8c72cc5fee97a42c52200ea59b4b8b2c3d95b3
Gérald Gounot <gerald@gounot.eu>
2025-08-06 18:56:38 +0200

    Ajout d'un composant MarkdownRenderer servant à centraliser le rendu avec @html

---
+++ front/src/lib/components/display/markdown-renderer.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { markdownToHTML } from "$lib/utils/misc";
+
+  interface Props {
+    content: string;
+    titleLevel?: number;
+  }
+
+  let { content, titleLevel }: Props = $props();
+
+  const html = markdownToHTML(content, titleLevel);
+</script>
+
+{@html html}
