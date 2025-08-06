810bb967276df461f8442587bd5bec53d57beaa5
Gérald Gounot <gerald@gounot.eu>
2025-08-06 14:22:28 +0200

    Ajout d'une fonction/constante DI_METABASE_DASHBOARD_URL avec l'URL vers le Metabase DI

--- front/src/lib/consts.ts
+++ front/src/lib/consts.ts
@@ -16,3 +16,6 @@ export const MON_RECAP_DEPARTMENTS = ["59", "69", "93"];
 
 export const METABASE_DASHBOARD_URL = (departmentCode: string) =>
   `https://metabase.dora.inclusion.gouv.fr/public/dashboard/cac884d0-fdeb-4d69-b1cc-9ae58a4cd32f?d%25C3%25A9partement_de_la_structure=${departmentCode}`;
+
+export const DI_METABASE_DASHBOARD_URL = (departmentName: string) =>
+  `https://stats.inclusion.beta.gouv.fr/public/dashboard/9a839a8a-2af2-4593-a800-1a67661247e8?d%25C3%25A9partement=${departmentName}`;
