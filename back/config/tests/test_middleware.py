b5396f35857b5e3ebfc1db2b4eadf01b95a36552
David Martin Cohen <davidmartincohen@gmail.com>
2025-09-04 15:33:36 +0200

    tests: ajouter des tests

---
+++ back/config/tests/test_middleware.py
@@ -0,0 +1,81 @@
+import os
+
+from django.http import HttpResponsePermanentRedirect
+from django.test import RequestFactory, TestCase, override_settings
+
+from config.domain_redirect_middleware import DomainRedirectMiddleware
+
+
+@override_settings(
+    OLD_DOMAIN="old-domain.com", NEW_DOMAIN="new-domain.com", ALLOWED_HOSTS=["*"]
+)
+class DomainRedirectMiddlewareTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.middleware = DomainRedirectMiddleware(get_response=lambda x: None)
+
+    def test_redirects_old_domain_to_new_domain(self):
+        request = self.factory.get("/api/test/", HTTP_HOST="old-domain.com")
+
+        response = self.middleware.process_request(request)
+
+        self.assertIsInstance(response, HttpResponsePermanentRedirect)
+        self.assertEqual(response.status_code, 308)
+        self.assertEqual(response["Location"], "https://new-domain.com/api/test/")
+
+    def test_preserves_query_parameters(self):
+        request = self.factory.get("/search/?q=test&page=2", HTTP_HOST="old-domain.com")
+
+        response = self.middleware.process_request(request)
+
+        self.assertEqual(
+            response["Location"], "https://new-domain.com/search/?q=test&page=2"
+        )
+
+    def test_preserves_post_data_with_308(self):
+        request = self.factory.post(
+            "/api/create/", {"name": "test"}, HTTP_HOST="old-domain.com"
+        )
+
+        response = self.middleware.process_request(request)
+
+        self.assertEqual(response.status_code, 308)
+
+    def test_no_redirect_for_new_domain(self):
+        request = self.factory.get("/api/test/", HTTP_HOST="new-domain.com")
+
+        response = self.middleware.process_request(request)
+
+        self.assertIsNone(response)
+
+    @override_settings(OLD_DOMAIN=None, NEW_DOMAIN=None)
+    def test_no_redirect_when_env_vars_missing(self):
+        os.environ.pop("OLD_DOMAIN", None)
+        os.environ.pop("NEW_DOMAIN", None)
+
+        request = self.factory.get("/api/test/", HTTP_HOST="old-domain.com")
+
+        response = self.middleware.process_request(request)
+
+        self.assertIsNone(response)
+
+    @override_settings(
+        OLD_DOMAIN="old-domain.com:8000",
+        NEW_DOMAIN="new-domain.com:8000",
+    )
+    def test_handles_different_ports(self):
+        os.environ["OLD_DOMAIN"] = "old-domain.com:8000"
+        os.environ["NEW_DOMAIN"] = "new-domain.com:8000"
+
+        request = self.factory.get("/api/test/", HTTP_HOST="old-domain.com:8000")
+
+        response = self.middleware.process_request(request)
+
+        self.assertEqual(response["Location"], "https://new-domain.com:8000/api/test/")
+
+    def test_handles_www_subdomain(self):
+        request = self.factory.get("/api/test/", HTTP_HOST="www.old-domain.com")
+
+        response = self.middleware.process_request(request)
+
+        self.assertIsNone(response)
