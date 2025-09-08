1c930af610642ead63364364595a01edb8f71ae4
Gérald Gounot <gerald@gounot.eu>
2025-09-08 18:36:40 +0200

    Suppression des éléments de menu : Ressources, Nouveautés et Communauté de l’inclusion

--- front/src/routes/_index/sub-menu.svelte
+++ front/src/routes/_index/sub-menu.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import ExternalLinkLineSystem from "svelte-remix/ExternalLinkLineSystem.svelte";
   import SubMenuDropdown from "./sub-menu-dropdown.svelte";
 
   interface Props {
@@ -10,7 +9,6 @@
 
   let {
     mobileDesign = false,
-    externalIconColor = mobileDesign ? "text-magenta-cta" : "text-gray-text",
     aClass = mobileDesign
       ? "py-s16 text-f18 font-bold text-gray-dark"
       : "text-f14 text-gray-text p-s16",
@@ -61,69 +59,6 @@
           },
         ]}
       />
-    </li>
-
-    <li>
-      <SubMenuDropdown
-        {mobileDesign}
-        label="Ressources"
-        links={[
-          {
-            href: "https://www.youtube.com/channel/UCadIsy9gfHgLmLkutRGC5ew/playlists",
-            label: "Tutoriels vidéo",
-            openInNewTab: true,
-          },
-          {
-            href: "https://aide.dora.inclusion.beta.gouv.fr/fr/article/participer-a-un-webinaire-dora-h3n747/",
-            label: "Webinaires",
-          },
-          {
-            href: "https://docs.google.com/presentation/d/e/2PACX-1vQ1xdf83eiY7CVTcWLpOg9cE8Zjd_9oEVe4RCeia9FNNB7FnTYXX6S21nbsMV3es6zgIwhwgKODI8dk/pub?start=false&loop=false&delayms=3000",
-            label: "Kit de déploiement",
-            openInNewTab: true,
-          },
-          {
-            href: "https://aide.dora.inclusion.beta.gouv.fr/fr/article/kit-de-communication-1x9t4kj/",
-            label: "Kit de communication",
-          },
-        ]}
-      />
-    </li>
-
-    <li class="border-gray-03 lg:border-none" class:border-b={mobileDesign}>
-      <a
-        target="_blank"
-        title="Ouverture dans une nouvelle fenêtre"
-        rel="noopener"
-        href="https://aide.dora.inclusion.beta.gouv.fr/fr/category/quoi-de-neuf-es1img/"
-        class="inline-block h-full {aClass}"
-      >
-        Nouveautés
-        <span
-          class="h-s20 w-s20 pl-s4 pt-s6 inline-block fill-current {externalIconColor}"
-          aria-hidden="true"
-        >
-          <ExternalLinkLineSystem size="16" />
-        </span>
-      </a>
-    </li>
-
-    <li class="border-gray-03 lg:border-none" class:border-b={mobileDesign}>
-      <a
-        target="_blank"
-        title="Ouverture dans une nouvelle fenêtre"
-        rel="noopener"
-        href="https://communaute.inclusion.beta.gouv.fr/forum/dora-105/"
-        class="inline-block h-full {aClass}"
-      >
-        Communauté de l’inclusion
-        <span
-          class="h-s20 w-s20 pl-s4 pt-s6 inline-block fill-current {externalIconColor}"
-          aria-hidden="true"
-        >
-          <ExternalLinkLineSystem size="16" />
-        </span>
-      </a>
     </li>
   </ul>
 </nav>
