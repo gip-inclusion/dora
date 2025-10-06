ad58c7cb4acdce5646760159c847ed2b6772121b
Gérald Gounot <gerald@gounot.eu>
2025-10-06 17:09:06 +0200

    choice.ts : corrige les types de retour

--- front/src/lib/utils/choice.ts
+++ front/src/lib/utils/choice.ts
@@ -1,10 +1,16 @@
 import type { Choice } from "$lib/types";
 
-export function getChoiceFromValue(value: string, choices: Choice[]): Choice {
+export function getChoiceFromValue(
+  value: string,
+  choices: Choice[]
+): Choice | undefined {
   return choices.find((choice) => choice.value === value);
 }
-export function getLabelFromValue(value: string, choices: Choice[]): string {
-  return getChoiceFromValue(value, choices).label;
+export function getLabelFromValue(
+  value: string,
+  choices: Choice[]
+): string | undefined {
+  return getChoiceFromValue(value, choices)?.label;
 }
 export function getCategoryKeyFromSubcategoryChoice(choice: Choice): string {
   return choice.value.split("--")[0];
