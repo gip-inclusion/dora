b40e97bb4035ac88530aeea9b514a122d570cf29
Gérald Gounot <gerald@gounot.eu>
2025-10-24 16:51:58 +0200

    Ajout d'une fonction utilitaire customFetch() centralisant la gestion de l'erreur 429

--- front/src/lib/utils/api.ts
+++ front/src/lib/utils/api.ts
@@ -1,5 +1,7 @@
+import { error } from "@sveltejs/kit";
 import { browser } from "$app/environment";
 import { API_URL, INTERNAL_API_URL } from "$lib/env";
+import * as Sentry from "@sentry/sveltekit";
 
 export function getApiURL() {
   if (browser || !INTERNAL_API_URL) {
@@ -9,3 +11,19 @@ export function getApiURL() {
 }
 
 export const defaultAcceptHeader = "application/json; version=1.0";
+
+export async function customFetch(
+  input: string | URL | globalThis.Request,
+  init?: RequestInit
+): Promise<Response> {
+  const response: Response = await fetch(input, init);
+
+  if (response.status === 429) {
+    Sentry.captureMessage(response.statusText);
+    // Erreur attendue 429 (rate limiting)
+    // Déclenche l'affichage du message d'erreur du fichier +error.svelte racine
+    error(429, response.statusText);
+  }
+
+  return response;
+}
