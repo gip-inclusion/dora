b45eaffc3db1274898ee18c2392d4f377c95a255
Gérald Gounot <gerald@gounot.eu>
2025-10-29 12:16:45 +0100

    Ajout d'un tableau de correspondance entre les modes de mobilisation DI et les modes d'orientation Dora

---
+++ back/dora/data_inclusion/constants.py
@@ -0,0 +1,8 @@
+from data_inclusion.schema.v1 import ModeMobilisation
+
+MODE_MOBILISATION_DI_TO_DORA = {
+    ModeMobilisation.ENVOYER_UN_COURRIEL: "envoyer-un-mail",
+    ModeMobilisation.SE_PRESENTER: "se-presenter",
+    ModeMobilisation.TELEPHONER: "telephoner",
+    ModeMobilisation.UTILISER_LIEN_MOBILISATION: "completer-le-formulaire-dadhesion",
+}
