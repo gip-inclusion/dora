e80cb69a69f5eb864ce94ea7c8667701b312e9cb
Gérald Gounot <gerald@gounot.eu>
2025-11-05 16:22:47 +0100

    test_service_di_date(): ne teste plus le champ date_suspension qui a été supprimé

--- back/dora/services/tests/test_services.py
+++ back/dora/services/tests/test_services.py
@@ -1776,7 +1776,6 @@ class DataInclusionSearchTestCase(APITestCase):
             date_creation="2022-01-01",
             date_maj="2023-01-01",
             recurrence="Tous les jours",
-            date_suspension="2030-01-01",
             structure={
                 "nom": "Rouge Empire",
                 "commune": "Sainte Jacquelineboeuf",
@@ -1793,9 +1792,6 @@ class DataInclusionSearchTestCase(APITestCase):
         self.assertEqual(response.data["modification_date"], service_data["date_maj"])
         self.assertEqual(response.data["publication_date"], None)
         self.assertEqual(response.data["recurrence"], service_data["recurrence"])
-        self.assertEqual(
-            response.data["suspension_date"], service_data["date_suspension"]
-        )
         self.assertEqual(response.data["publication_date"], None)
 
     def test_service_di_structure(self):
