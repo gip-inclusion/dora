e54a62608bcf91ce4fa62c6f9924eb4dca4f9de6
David Martin Cohen <davidmartincohen@gmail.com>
2025-11-12 16:19:02 +0100

    feat: remplacer MatomoOptOut avec gestionnaire de cookie

--- front/src/routes/(static)/politique-de-confidentialite/+page.svelte
+++ front/src/routes/(static)/politique-de-confidentialite/+page.svelte
@@ -2,8 +2,8 @@
   import Breadcrumb from "$lib/components/display/breadcrumb.svelte";
   import CenteredGrid from "$lib/components/display/centered-grid.svelte";
   import StaticMarkdown from "$lib/components/display/static-markdown.svelte";
-  import MatomoOptOut from "./matomo-opt-out.svelte";
   import type { PageData } from "./$types";
+  import { showCookieBanner } from "$lib/utils/consent.svelte";
 
   interface Props {
     data: PageData;
@@ -20,6 +20,21 @@
   <StaticMarkdown content={data.content} />
 
   <div class="mt-s48 max-w-3xl">
-    <MatomoOptOut />
+    <p>
+      DORA utilise également la solution de mesure d'audience
+      <a href="https://matomo.org/" class="underline"> Matomo </a>
+
+      en l'ayant configuré en mode « exempté », conformément aux
+      <a
+        href="https://www.cnil.fr/fr/solutions-pour-la-mesure-daudience"
+        class="underline"
+      >
+        recommandations de la CNIL</a
+      >. Elle ne nécessite donc pas le consentement des personnes concernées.
+      Vous pouvez malgré tout vous opposer au suivi de votre navigation via
+      <a class="text-magenta-cta underline" onclick={showCookieBanner}
+        >le gestionnaire de cookies.</a
+      >
+    </p>
   </div>
 </CenteredGrid>
