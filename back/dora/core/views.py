55ab161c4f9c09882d09472dc29528c2736471d4
David Martin Cohen <davidmartincohen@gmail.com>
2025-11-17 16:36:59 +0100

    fix: utiliser filename de l'url pour validation

--- back/dora/core/views.py
+++ back/dora/core/views.py
@@ -18,10 +18,10 @@ from dora.structures.models import Structure
 logger = logging.getLogger(__name__)
 
 
-def _validate_upload(file_obj):
-    if file_obj.size > settings.MAX_UPLOAD_SIZE_MB * 1024 * 1024:
+def _validate_upload(file_name, file_size):
+    if file_size > settings.MAX_UPLOAD_SIZE_MB * 1024 * 1024:
         raise ValidationError("FILE_TOO_BIG")
-    if file_obj.name.split(".")[-1] not in settings.ALLOWED_UPLOADED_FILES_EXTENSIONS:
+    if file_name.split(".")[-1] not in settings.ALLOWED_UPLOADED_FILES_EXTENSIONS:
         raise ValidationError("INVALID_EXTENSION")
 
 
@@ -31,7 +31,7 @@ def _validate_upload(file_obj):
 def upload(request, filename, structure_slug):
     structure = get_object_or_404(Structure.objects.all(), slug=structure_slug)
     file_obj = request.data["file"]
-    _validate_upload(file_obj)
+    _validate_upload(filename, file_obj.size)
     clean_filename = (
         f"{settings.ENVIRONMENT}/{structure.pk}/{get_valid_filename(filename)}"
     )
@@ -44,7 +44,7 @@ def upload(request, filename, structure_slug):
 @permission_classes([IsAuthenticated])
 def safe_upload(request, filename):
     file_obj = request.data["file"]
-    _validate_upload(file_obj)
+    _validate_upload(filename, file_obj.size)
     clean_filename = f"{settings.ENVIRONMENT}/#orientations/{get_random_string(32)}/{get_valid_filename(filename)}"
     result = default_storage.save(clean_filename, file_obj)
     return Response({"key": result}, status=201)
