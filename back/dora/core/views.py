32d5ceeb8daf2ebaa536287d432c92959e8f2837
David Martin Cohen <davidmartincohen@gmail.com>
2025-11-17 16:51:04 +0100

    refactor: ajouter des types aux vues

--- back/dora/core/views.py
+++ back/dora/core/views.py
@@ -10,6 +10,7 @@ from rest_framework.decorators import api_view, parser_classes, permission_class
 from rest_framework.exceptions import ValidationError
 from rest_framework.parsers import FileUploadParser
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
 from rest_framework.response import Response
 
 from dora.services.models import Service
@@ -18,7 +19,7 @@ from dora.structures.models import Structure
 logger = logging.getLogger(__name__)
 
 
-def _validate_upload(file_name, file_size):
+def _validate_upload(file_name: str, file_size: int) -> None:
     if file_size > settings.MAX_UPLOAD_SIZE_MB * 1024 * 1024:
         raise ValidationError("FILE_TOO_BIG")
     if file_name.split(".")[-1] not in settings.ALLOWED_UPLOADED_FILES_EXTENSIONS:
@@ -28,7 +29,7 @@ def _validate_upload(file_name, file_size):
 @api_view(["POST"])
 @parser_classes([FileUploadParser])
 @permission_classes([IsAuthenticated])
-def upload(request, filename, structure_slug):
+def upload(request: Request, filename: str, structure_slug: str) -> Response:
     structure = get_object_or_404(Structure.objects.all(), slug=structure_slug)
     file_obj = request.data["file"]
     _validate_upload(filename, file_obj.size)
@@ -42,7 +43,7 @@ def upload(request, filename, structure_slug):
 @api_view(["POST"])
 @parser_classes([FileUploadParser])
 @permission_classes([IsAuthenticated])
-def safe_upload(request, filename):
+def safe_upload(request: Request, filename: str) -> Response:
     file_obj = request.data["file"]
     _validate_upload(filename, file_obj.size)
     clean_filename = f"{settings.ENVIRONMENT}/#orientations/{get_random_string(32)}/{get_valid_filename(filename)}"
@@ -52,13 +53,13 @@ def safe_upload(request, filename):
 
 @api_view()
 @permission_classes([permissions.AllowAny])
-def ping(request):
+def ping(request: Request) -> Response:
     check_services = Service.objects.exists()
     if check_services:
         return Response("ok", status=200)
     return Response("ko", status=500)
 
 
-def trigger_error(request):
+def trigger_error(request: Request) -> None:
     division_by_zero = 1 / 0
     print(division_by_zero)
