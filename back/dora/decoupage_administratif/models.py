cc5f3298dd96cacc4aa76fc19a079a76cfedab13
Gérald Gounot <gerald@gounot.eu>
2025-11-20 16:37:54 +0100

    Création des modèles de découpage administratif

--- back/dora/decoupage_administratif/models.py
+++ back/dora/decoupage_administratif/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class Commune(models.Model):
+    nom = models.CharField(max_length=255)
+    code = models.CharField(max_length=5, unique=True)
+    code_departement = models.CharField(max_length=3)
+    code_epci = models.CharField(max_length=9)
+    code_region = models.CharField(max_length=3)
+    codes_postaux = models.JSONField(default=list)
+
+    class Meta:
+        verbose_name = "commune"
+        verbose_name_plural = "communes"
+
+    def __str__(self) -> str:
+        return f"{self.nom} ({self.code})"
+
+
+class Departement(models.Model):
+    nom = models.CharField(max_length=255)
+    code = models.CharField(max_length=3, unique=True)
+    code_region = models.CharField(max_length=3)
+
+    class Meta:
+        verbose_name = "département"
+        verbose_name_plural = "départements"
+
+    def __str__(self) -> str:
+        return f"{self.nom} ({self.code})"
+
+
+class Epci(models.Model):
+    nom = models.CharField(max_length=255)
+    code = models.CharField(max_length=9, unique=True)
+    codes_departements = models.JSONField(default=list)
+    codes_regions = models.JSONField(default=list)
+
+    class Meta:
+        verbose_name = "EPCI"
+        verbose_name_plural = "EPCI"
+
+    def __str__(self) -> str:
+        return f"{self.nom} ({self.code})"
+
+
+class Region(models.Model):
+    nom = models.CharField(max_length=255)
+    code = models.CharField(max_length=3, unique=True)
+
+    class Meta:
+        verbose_name = "région"
+        verbose_name_plural = "régions"
+
+    def __str__(self) -> str:
+        return f"{self.nom} ({self.code})"
