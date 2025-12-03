c9663a14f816ed38c712f88266fddd8a8de269ed
David Martin Cohen <davidmartincohen@gmail.com>
2025-12-03 17:50:40 +0100

    feat: mettre à jour le processing_date d'une orientation après la modération

--- back/dora/structures/admin.py
+++ back/dora/structures/admin.py
@@ -350,6 +350,7 @@ class StructureAdmin(BaseImportAdminMixin, admin.ModelAdmin):
 
         # Passage des demandes d'orientation en attente au statut Ouverte / En cours de traitement et envoi des e-mails
         for orientation in moderation_pending_orientations:
+            orientation.processing_date = timezone.now()
             orientation.status = OrientationStatus.PENDING
             orientation.save()
             send_orientation_created_emails(orientation)
