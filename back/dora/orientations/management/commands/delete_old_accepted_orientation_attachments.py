31d92134d21ce985fd90a61c8f6af37c135d83ef
Gérald Gounot <gerald@gounot.eu>
2025-12-18 18:47:23 +0100

    Ajout d'une commande de suppression des pièces jointes des orientations acceptées après un délai

---
+++ back/dora/orientations/management/commands/delete_old_accepted_orientation_attachments.py
@@ -0,0 +1,49 @@
+from dateutil.relativedelta import relativedelta
+from django.conf import settings
+from django.db import transaction
+from django.utils import timezone
+
+from dora.core.commands import BaseCommand
+from dora.orientations.models import Orientation, OrientationStatus
+
+
+class Command(BaseCommand):
+    help = "Supprime les pièces-jointes de orientations acceptées après un délai"
+
+    def handle(self, *args, **options):
+        self.logger.info(
+            "Suppression des pièces-jointes des orientations acceptées il y a plus de %d mois.",
+            settings.ORIENTATION_ATTACHMENTS_EXPIRATION_PERIOD_MONTHS,
+        )
+
+        expiration_date = timezone.localdate() - relativedelta(
+            months=settings.ORIENTATION_ATTACHMENTS_EXPIRATION_PERIOD_MONTHS
+        )
+
+        orientations = Orientation.objects.filter(
+            processing_date__lte=expiration_date,
+            status=OrientationStatus.ACCEPTED,
+        ).select_related("service")
+
+        for orientation in orientations:
+            try:
+                with transaction.atomic():
+                    orientation.delete_attachments()
+
+                    self.logger.info(
+                        "Les pièces-jointes de l'orientation %s ont été supprimées.",
+                        orientation.pk,
+                    )
+
+            except Exception as e:
+                self.logger.warning(
+                    "Erreur lors de la suppression des pièces-jointes de l'orientation %s: %s",
+                    orientation.id,
+                    e,
+                )
+
+        self.logger.info(
+            "%d orientations acceptées il y a plus de %d mois ont vu leurs pièces-jointes supprimées.",
+            len(orientations),
+            settings.ORIENTATION_ATTACHMENTS_EXPIRATION_PERIOD_MONTHS,
+        )
