588471d055c9b1a6eb477bf3b9e8b253b369863d
Gérald Gounot <gerald@gounot.eu>
2025-12-19 17:40:35 +0100

    Update script message for closed orientations

--- back/tools/delete-old-accepted-orientation-attachments.sh
+++ back/tools/delete-old-accepted-orientation-attachments.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "Suppression des pièces jointes des anciennes orientations acceptées"
+echo "Suppression des pièces jointes des anciennes orientations fermées"
 python /app/manage.py delete_old_closed_orientation_attachments
