be1fab2f09d5c9f2caddc815117b94f5c6e57dc2
Gérald Gounot <gerald@gounot.eu>
2026-01-12 17:58:40 +0100

    Ajout du filtre sur les publics DI

--- front/src/routes/recherche/result-filters.svelte
+++ front/src/routes/recherche/result-filters.svelte
@@ -7,6 +7,7 @@
   } from "$lib/types";
 
   export interface Filters {
+    diPublics: Array<string>;
     kinds: Array<ServiceKind>;
     fundingLabels: Array<FundingLabel["value"]>;
     feeConditions: Array<FeeCondition>;
@@ -33,6 +34,12 @@
 </script>
 
 <div class="gap-s32 flex flex-col">
+  <ResultFilter
+    id="diPublics"
+    label="Publics"
+    choices={servicesOptions.diPublics}
+    bind:group={filters.diPublics}
+  />
   <ResultFilter
     id="kinds"
     label="Type de service"
