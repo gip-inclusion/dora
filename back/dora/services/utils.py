4d9163880efa4462f00a02134747d4bc1f15b51b
Gérald Gounot <gerald@gounot.eu>
2026-01-28 17:53:03 +0100

    services/utils: utilise les modèles de decoupage_administratif

--- back/dora/services/utils.py
+++ back/dora/services/utils.py
@@ -5,10 +5,11 @@ from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 
-from dora.admin_express.models import EPCI, AdminDivisionType, City, Department, Region
+from dora.admin_express.models import AdminDivisionType
 from dora.admin_express.utils import arrdt_to_main_insee_code
 from dora.core.constants import WGS84
 from dora.core.models import ModerationStatus
+from dora.decoupage_administratif.models import EPCI, City, Department, Region
 from dora.services.enums import ServiceStatus
 
 SYNC_FIELDS = [
@@ -152,7 +153,7 @@ def filter_services_by_city_code(services, city_code):
         )
         | (
             Q(diffusion_zone_type=AdminDivisionType.EPCI)
-            & Q(diffusion_zone_details__in=city.epcis)
+            & Q(diffusion_zone_details=city.epci)
         )
         | (
             Q(diffusion_zone_type=AdminDivisionType.DEPARTMENT)
