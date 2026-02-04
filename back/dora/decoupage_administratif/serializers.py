095cfdad568f8602bec5d895d70b5bf235ac8981
Gérald Gounot <gerald@gounot.eu>
2026-02-04 17:37:38 +0100

    Décomposition de AdminDivisionSerializer en AdminDivisionSerializer et AdminDivisionSearchResultSerializer

--- back/dora/decoupage_administratif/serializers.py
+++ back/dora/decoupage_administratif/serializers.py
@@ -19,4 +19,7 @@ class SearchQuerySerializer(serializers.Serializer):
 class AdminDivisionSerializer(serializers.Serializer):
     code = serializers.CharField()
     name = serializers.CharField()
+
+
+class AdminDivisionSearchResultSerializer(AdminDivisionSerializer):
     similarity = serializers.FloatField()
--- back/dora/decoupage_administratif/views.py
+++ back/dora/decoupage_administratif/views.py
@@ -8,7 +8,7 @@ from dora.decoupage_administratif.models import AdminDivisionType
 from dora.decoupage_administratif.utils import normalize_string_for_search
 
 from .models import EPCI, City, Department, Region
-from .serializers import AdminDivisionSerializer, SearchQuerySerializer
+from .serializers import AdminDivisionSearchResultSerializer, SearchQuerySerializer
 
 
 @api_view(["GET"])
@@ -47,4 +47,4 @@ def search(request):
             .order_by(*sort_fields)[:10]
         )
 
-    return Response(AdminDivisionSerializer(qs, many=True).data)
+    return Response(AdminDivisionSearchResultSerializer(qs, many=True).data)
