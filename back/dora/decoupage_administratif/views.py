5cbb60b6d206f828811071fe68b3a66dd509e613
Gérald Gounot <gerald@gounot.eu>
2026-02-05 14:38:17 +0100

    feat: Utilisation des données de Découpage Administratif pour la vue d'obtention du nom d'une commune à partir de son code Insee

--- back/dora/decoupage_administratif/views.py
+++ back/dora/decoupage_administratif/views.py
@@ -109,3 +109,12 @@ def get_departments(request):
         departments = Department.objects.all().order_by("code")
 
     return Response(AdminDivisionSerializer(departments, many=True).data)
+
+
+@api_view(["GET"])
+@permission_classes([permissions.AllowAny])
+def get_city_label(request, insee_code):
+    city = City.objects.get_from_code(insee_code.upper())
+    if city:
+        return Response(city.name)
+    raise NotFound
