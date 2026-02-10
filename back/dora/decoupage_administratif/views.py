e5326d6db7d96b7f6ddf0d2e4b8beefcea408513
Gérald Gounot <gerald@gounot.eu>
2026-02-10 13:55:18 +0100

    get_city_label:: utilisation du walrus operator

--- back/dora/decoupage_administratif/views.py
+++ back/dora/decoupage_administratif/views.py
@@ -114,7 +114,6 @@ def get_departments(request):
 @api_view(["GET"])
 @permission_classes([permissions.AllowAny])
 def get_city_label(request, insee_code):
-    city = City.objects.get_from_code(insee_code)
-    if city:
+    if city := City.objects.get_from_code(insee_code):
         return Response(city.name)
     raise NotFound
