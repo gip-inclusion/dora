aa876d0fe8ad9f07a15eb393d5647d8f2ceae390
Gérald Gounot <gerald@gounot.eu>
2026-02-10 17:22:06 +0100

    Réécris les tests en utilisant pytest

--- back/dora/decoupage_administratif/tests/test_get_city_label.py
+++ back/dora/decoupage_administratif/tests/test_get_city_label.py
@@ -1,76 +1,34 @@
-from django.contrib.gis.geos import Point
-from django.test import TestCase
-from rest_framework.test import APIClient
-
-from dora.core.constants import WGS84
-from dora.decoupage_administratif.models import City
+import pytest
 
 
-class GetCityLabelViewTests(TestCase):
-    """Tests pour l'endpoint /city-label/<insee_code>/ (vue get_city_label)."""
+@pytest.mark.parametrize(
+    "insee_code,expected_name",
+    [
+        ("75056", "Paris"),
+        ("69123", "Lyon"),
+        ("2A004", "Ajaccio"),
+        ("2a004", "Ajaccio"),
+    ],
+)
+@pytest.mark.django_db
+def test_get_city_label_returns_city_name(
+    client, test_cities, insee_code, expected_name
+):
+    """Test que l'endpoint retourne le nom de la ville pour un code INSEE valide."""
+    response = client.get(f"/city-label/{insee_code}/")
+    assert response.status_code == 200
+    assert response.json() == expected_name
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.client = APIClient()
 
-        cls.city_paris = City.objects.create(
-            code="75056",
-            name="Paris",
-            department="75",
-            region="11",
-            epci="200055781",
-            normalized_name="PARIS",
-            center=Point(2.3522, 48.8566, srid=WGS84),
-        )
-        cls.city_lyon = City.objects.create(
-            code="69123",
-            name="Lyon",
-            department="69",
-            region="84",
-            epci="200046977",
-            normalized_name="LYON",
-            center=Point(4.8357, 45.7640, srid=WGS84),
-        )
-        cls.city_ajaccio = City.objects.create(
-            code="2A004",
-            name="Ajaccio",
-            department="2A",
-            region="94",
-            epci="200040947",
-            normalized_name="AJACCIO",
-            center=Point(8.7386, 41.9258, srid=WGS84),
-        )
-
-    def test_get_city_label_returns_city_name(self):
-        """Le code 75056 retourne Paris."""
-        response = self.client.get("/city-label/75056/")
-        assert response.status_code == 200
-        assert response.json() == "Paris"
-
-    def test_get_city_label_lyon(self):
-        """ "Le code 69123 retourne Lyon."""
-        response = self.client.get("/city-label/69123/")
-        assert response.status_code == 200
-        assert response.json() == "Lyon"
-
-    def test_get_city_label_corse_2a_uppercase(self):
-        """Un code INSEE Corse en majuscules (2A) est correctement reconnu."""
-        response = self.client.get("/city-label/2A004/")
-        assert response.status_code == 200
-        assert response.json() == "Ajaccio"
-
-    def test_get_city_label_corse_2a_lowercase(self):
-        """Un code INSEE Corse en minuscules (2a) est correctement reconnu."""
-        response = self.client.get("/city-label/2a004/")
-        assert response.status_code == 200
-        assert response.json() == "Ajaccio"
-
-    def test_get_city_label_unknown_insee_returns_404(self):
-        """Un code INSEE inconnu retourne 404."""
-        response = self.client.get("/city-label/99999/")
-        assert response.status_code == 404
-
-    def test_get_city_label_invalid_code_returns_404(self):
-        """Un code INSEE invalide retourne 404."""
-        response = self.client.get("/city-label/0/")
-        assert response.status_code == 404
+@pytest.mark.parametrize(
+    "insee_code",
+    [
+        "99999",
+        "0",
+    ],
+)
+@pytest.mark.django_db
+def test_get_city_label_returns_404(client, insee_code):
+    """Test que l'endpoint retourne 404 pour un code INSEE invalide ou inconnu."""
+    response = client.get(f"/city-label/{insee_code}/")
+    assert response.status_code == 404
