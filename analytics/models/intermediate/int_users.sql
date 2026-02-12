a3c473629277d8dda9e2c57cc6fc1bae28719673
Giulia Carra <giulia.carra@gmail.com>
2026-02-12 17:06:30 +0100

    add structure membership to dim_user

--- analytics/models/intermediate/int_users.sql
+++ analytics/models/intermediate/int_users.sql
@@ -12,11 +12,24 @@ WITH usr AS (
 
 usr_with_imer AS (
     SELECT * FROM {{ ref("int_users_with_iMer") }}
+),
+
+structure_members AS (
+    SELECT
+        user_id,
+        MIN(creation_date) AS first_date_as_structure_member
+    FROM {{ source('dora', 'structures_structuremember') }}
+    GROUP BY user_id
+
 )
 
 SELECT
     usr.*,
-    usr_with_imer.user_id IS NOT NULL AS user_with_imer
+    usr_with_imer.user_id IS NOT NULL     AS user_with_imer,
+    structure_members.first_date_as_structure_member,
+    structure_members.user_id IS NOT NULL AS user_is_structure_member
 FROM usr
 LEFT JOIN usr_with_imer
     ON usr.id = usr_with_imer.user_id
+LEFT JOIN structure_members
+    ON usr.id = structure_members.user_id
