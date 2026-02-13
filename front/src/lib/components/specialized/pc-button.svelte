c8cb0d7e33aefc3f6edee08ac03466dcdc81b2e1
Gérald Gounot <gerald@gounot.eu>
2026-02-13 15:19:36 +0100

    PcButton: ajout d'une prop pour cacher la section aide

--- front/src/lib/components/specialized/pc-button.svelte
+++ front/src/lib/components/specialized/pc-button.svelte
@@ -7,42 +7,45 @@
 
   interface Props {
     nextPage: string;
+    noHelpSection?: boolean;
     pcHelpLink?: Snippet;
   }
 
-  let { nextPage, pcHelpLink }: Props = $props();
+  let { nextPage, noHelpSection = false, pcHelpLink }: Props = $props();
 
   const loginUrl = `${getApiURL()}/oidc/login/?next=${encodeURIComponent(nextPage)}`;
 </script>
 
 <div class="text-center">
-  <a class="mb-s24 inline-block" href={loginUrl}>
+  <a class={[!noHelpSection && "mb-s24", "inline-block"]} href={loginUrl}>
     <img src={logoPC} alt="" class="max-w-none" />
   </a>
 
-  <div class="text-center">
-    {#if pcHelpLink}
-      {@render pcHelpLink()}
-    {:else}
+  {#if !noHelpSection}
+    <div class="text-center">
+      {#if pcHelpLink}
+        {@render pcHelpLink()}
+      {:else}
+        <a
+          class="text-magenta-cta underline"
+          target="_blank"
+          title="Aide DORA - ouverture dans une nouvelle fenêtre"
+          rel="noopener noreferrer"
+          href={`${URL_HELP_SITE}category/inscription-et-gestion-du-compte-ha8m5b/`}
+        >
+          Besoin d’aide&#8239;? Contactez-nous
+        </a>
+      {/if}
+      &nbsp;
       <a
         class="text-magenta-cta underline"
         target="_blank"
-        title="Aide DORA - ouverture dans une nouvelle fenêtre"
+        title="Qu’est-ce que ProConnect&#8239;? nouvelle fenêtre"
         rel="noopener noreferrer"
-        href={`${URL_HELP_SITE}category/inscription-et-gestion-du-compte-ha8m5b/`}
+        href="https://www.proconnect.gouv.fr/"
       >
-        Besoin d’aide&#8239;? Contactez-nous
+        Qu'est que ProConnect&#8239;?
       </a>
-    {/if}
-    &nbsp;
-    <a
-      class="text-magenta-cta underline"
-      target="_blank"
-      title="Qu’est-ce que ProConnect&#8239;? nouvelle fenêtre"
-      rel="noopener noreferrer"
-      href="https://www.proconnect.gouv.fr/"
-    >
-      Qu'est que ProConnect&#8239;?
-    </a>
-  </div>
+    </div>
+  {/if}
 </div>
