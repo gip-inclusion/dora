054137e019e09dc9f0c1265ce3094a4d5a13c63b
Gérald Gounot <gerald@gounot.eu>
2026-02-13 19:31:01 +0100

    Ajout d'une constante stockant les caractéristiques des services

---
+++ front/src/routes/_index/menu-nexus/consts.ts
@@ -0,0 +1,40 @@
+import LogoLesEmplois from "$lib/assets/logos/logo-emploi-inclusion.svg";
+import LogoDoraFull from "$lib/assets/logos/logo-dora-full.svg";
+import LogoLeMarche from "$lib/assets/logos/logo-marche-inclusion.svg";
+import LogoPilotage from "$lib/assets/logos/logo-pilotage-inclusion.svg";
+import LogoMonRecap from "$lib/assets/logos/logo-monrecap.svg";
+
+import type { NexusService } from "./types";
+
+export const ALL_SERVICES: NexusService[] = [
+  {
+    id: "les-emplois",
+    label: "Les Emplois de l’inclusion",
+    url: "https://emplois.inclusion.beta.gouv.fr/dashboard/",
+    icon: LogoLesEmplois,
+  },
+  {
+    id: "dora",
+    label: "DORA",
+    url: "https://dora.inclusion.gouv.fr/structures/",
+    icon: LogoDoraFull,
+  },
+  {
+    id: "le-marche",
+    label: "Le Marché de l’inclusion",
+    url: "https://lemarche.inclusion.gouv.fr/profil/",
+    icon: LogoLeMarche,
+  },
+  {
+    id: "pilotage",
+    label: "Le Pilotage de l’inclusion",
+    url: "https://emplois.inclusion.beta.gouv.fr/dashboard/",
+    icon: LogoPilotage,
+  },
+  {
+    id: "mon-recap",
+    label: "Mon Récap",
+    url: "https://mon-recap.inclusion.beta.gouv.fr/formulaire-commande-carnets/",
+    icon: LogoMonRecap,
+  },
+];
