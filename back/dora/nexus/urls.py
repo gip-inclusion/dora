c3f10a34cb5df271e70ac03b7b57d7bd0933171f
Gérald Gounot <gerald@gounot.eu>
2026-02-17 15:30:34 +0100

    Ajout de l'URL du endpoint

--- back/dora/nexus/urls.py
+++ back/dora/nexus/urls.py
@@ -5,4 +5,5 @@ from . import views
 nexus_patterns = [
     path("nexus/auto-login-in/", views.auto_login_in, name="auto-login-in"),
     path("nexus/auto-login-out/", views.auto_login_out, name="auto-login-out"),
+    path("nexus/dropdown-status/", views.dropdown_status, name="dropdown-status"),
 ]
