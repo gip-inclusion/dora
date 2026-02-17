5201f37ef7d5368a2ae6668855895ea76772bffc
Gérald Gounot <gerald@gounot.eu>
2026-02-17 15:31:26 +0100

    Ajout d'une fonction côté front permettant de récupérer le statut du menu Nexus

---
+++ front/src/lib/requests/nexus.ts
@@ -0,0 +1,30 @@
+import { getApiURL } from "$lib/utils/api";
+import { getToken } from "$lib/utils/auth";
+
+export type NexusServiceID =
+  | "dora"
+  | "les-emplois"
+  | "le-marche"
+  | "mon-recap"
+  | "pilotage";
+
+export type NexusDropDownStatus = {
+  proconnect: boolean;
+  activatedServices: NexusServiceID[];
+  mvpEnabled: boolean;
+};
+
+export const getNexusDropdownStatus = async () => {
+  const url = new URL("/nexus/dropdown-status/", getApiURL());
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${getToken()}`,
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch Nexus dropdown status");
+  }
+  return response.json() as Promise<NexusDropDownStatus>;
+};
