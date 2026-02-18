b70559b81e33955bf4ffa3427edd9c60ed78146b
Gérald Gounot <gerald@gounot.eu>
2026-02-18 18:03:40 +0100

    Mutualisation du code des menus d'inscription

--- front/src/routes/_index/menu-nexus/menu-inscription-emplois.svelte
+++ front/src/routes/_index/menu-nexus/menu-inscription-emplois.svelte
@@ -1,50 +1,18 @@
 <script lang="ts">
-  import ExternalLinkLineSystem from "svelte-remix/ExternalLinkLineSystem.svelte";
-
-  import logoNexus from "$lib/assets/logos/logo-nexus.png";
-
   import { EMPLOIS_SIGNUP_PAGE_URL } from "$lib/env";
 
   import LinkButton from "$lib/components/display/link-button.svelte";
 
-  import { EMPLOIS_MORE_INFO_URL } from "$lib/consts";
+  import MenuInscriptionBase from "./menu-inscription-base.svelte";
 </script>
 
-<div class="py-s4 px-s12 flex flex-col items-center lg:w-[327px]">
-  <div class="my-s8 flex items-center justify-center">
-    <img src={logoNexus} alt="Plateforme de l’inclusion" class="w-[197px]" />
-  </div>
-
-  <h4 class="mb-s16 text-f18 text-france-blue leading-24 font-bold">
-    Découvrez votre nouveau portail unifié&#8239;!
-  </h4>
-
-  <p class="mb-s16 text-f14 text-gray-text leading-24">
-    Accédez bientôt à l’ensemble de votre offre et de vos informations.
-  </p>
-
-  <hr class="mb-s16 border-gray-01 w-full" />
-
-  <p class="mb-s16 text-f14 text-gray-text leading-24">
-    Pour accéder à votre portail, vous devez avoir un compte sur les Emplois de
-    l’inclusion
-  </p>
-
+<MenuInscriptionBase
+  text="Pour accéder à votre portail, vous devez avoir un compte sur les Emplois de l’inclusion"
+>
   <LinkButton
     label="S’inscrire aux Emplois de l’inclusion"
     to={EMPLOIS_SIGNUP_PAGE_URL}
     wFull
     extraClass="mb-s16"
   />
-
-  <LinkButton
-    label="En savoir plus"
-    to={EMPLOIS_MORE_INFO_URL}
-    small
-    otherTab
-    nofollow
-    noBackground
-    icon={ExternalLinkLineSystem}
-    iconOnRight
-  />
-</div>
+</MenuInscriptionBase>
--- front/src/routes/_index/menu-nexus/menu-inscription-proconnect.svelte
+++ front/src/routes/_index/menu-nexus/menu-inscription-proconnect.svelte
@@ -1,51 +1,18 @@
 <script lang="ts">
-  import ExternalLinkLineSystem from "svelte-remix/ExternalLinkLineSystem.svelte";
-
   import { page } from "$app/state";
 
-  import logoNexus from "$lib/assets/logos/logo-nexus.png";
-
-  import LinkButton from "$lib/components/display/link-button.svelte";
   import PcButton from "$lib/components/specialized/pc-button.svelte";
 
-  import { PROCONNECT_MORE_INFO_URL } from "$lib/consts";
-
   import { getNextPage } from "../../auth/utils";
+  import MenuInscriptionBase from "./menu-inscription-base.svelte";
 
   const nextPage = getNextPage(page.url);
 </script>
 
-<div class="py-s4 px-s12 flex flex-col items-center lg:w-[303px]">
-  <div class="my-s8 flex items-center justify-center">
-    <img src={logoNexus} alt="Plateforme de l’inclusion" class="w-[197px]" />
-  </div>
-
-  <h4 class="mb-s16 text-f18 text-france-blue leading-24 font-bold">
-    Découvrez votre nouveau portail unifié&#8239;!
-  </h4>
-
-  <p class="mb-s16 text-f14 text-gray-text leading-24">
-    Accédez bientôt à l’ensemble de votre offre et de vos informations.
-  </p>
-
-  <hr class="mb-s16 border-gray-01 w-full" />
-
-  <p class="mb-s16 text-f14 text-gray-text leading-24">
-    Pour accéder à votre portail, vous devez avoir un compte ProConnect.
-  </p>
-
+<MenuInscriptionBase
+  text="Pour accéder à votre portail, vous devez avoir un compte ProConnect."
+>
   <div class="mb-s16">
     <PcButton {nextPage} noHelpSection />
   </div>
-
-  <LinkButton
-    label="En savoir plus"
-    to={PROCONNECT_MORE_INFO_URL}
-    small
-    otherTab
-    nofollow
-    noBackground
-    icon={ExternalLinkLineSystem}
-    iconOnRight
-  />
-</div>
+</MenuInscriptionBase>
