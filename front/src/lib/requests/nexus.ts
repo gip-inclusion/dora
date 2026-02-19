49a0105d2704c99f9aad39ae1435747f5178fc1e
Gérald Gounot <gerald@gounot.eu>
2026-02-19 19:16:49 +0100

    frontend: ajout d'une fonction d'appel du endpoint de décodage des infos bénéficiaire

--- front/src/lib/requests/nexus.ts
+++ front/src/lib/requests/nexus.ts
@@ -28,3 +28,28 @@ export const getNexusMenuStatus = async () => {
   }
   return response.json() as Promise<NexusMenuStatus>;
 };
+
+export type OrientationBeneficiaryInfo = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  franceTravailId: string;
+};
+
+export const getOrientationBeneficiaryInfo = async (opJwt: string) => {
+  const url = new URL("/orientations/emplois/beneficiary-info/", getApiURL());
+  url.searchParams.set("op", opJwt);
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json; version=1.0",
+      Authorization: `Token ${getToken()}`,
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch orientation beneficiary info");
+  }
+  return response.json() as Promise<OrientationBeneficiaryInfo>;
+};
