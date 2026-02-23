98f1ce28b9979c29630a00c9f3b7ed3283989027
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-02-23 15:18:10 +0100

    back: Split nexus tasks in two
    We want to sync demo data

--- back/tools/sync-nexus.sh
+++ back/tools/sync-nexus.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+echo "Synchronisation des données Nexus"
+python /app/manage.py nexus_full_sync
+
 ## Only run on the production app
 if [ "$ENVIRONMENT" != "production" ];then
-  echo "La synchronisation des utilisateurs pour Nexus ne se fait qu'en production"
+  echo "La synchronisation metabase pour Nexus ne se fait qu'en production"
   exit 0;
 fi
 
-echo "Synchronisation des utilisateurs pour Nexus"
-python /app/manage.py nexus_full_sync
+echo "Synchronisation metabase pour Nexus"
 python /app/manage.py populate_metabase_nexus
