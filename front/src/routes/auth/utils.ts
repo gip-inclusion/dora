36fefdaba70a8f32a93fc45746df1d91913638e5
David Martin Cohen <david.cohen@inclusion.gouv.fr>
2026-03-05 18:15:23 +0100

    Apply suggestion from @francoisfreitag
    Co-authored-by: François Freitag <mail@franek.fr>

--- front/src/routes/auth/utils.ts
+++ front/src/routes/auth/utils.ts
@@ -2,7 +2,7 @@ export function getNextPage(url: URL) {
   const next = url.searchParams.get("next");
   const basePath = next && next.startsWith("/") ? next : "/";
 
-  // Préserver les query params (sauf "next") dans l'url renovoyé
+  // Préserver les query params (sauf "next") dans l'url renvoyé
   const paramsToForward = new URLSearchParams();
   for (const [key, value] of url.searchParams) {
     if (key !== "next") {
