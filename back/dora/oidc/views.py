ee62dbb67498311d9fd48b0c19c6366cb32a8643
David Martin Cohen <davidmartincohen@gmail.com>
2026-03-09 18:51:52 +0100

    refactor: extraire _sanitize_next_url()

--- back/dora/oidc/views.py
+++ back/dora/oidc/views.py
@@ -26,6 +26,18 @@ from dora.core.utils import set_auth_token_cookie
 logger = logging.getLogger(__name__)
 
 
+def _sanitize_next_url(next_url: str) -> str | None:
+    try:
+        parsed_next = urlparse(next_url)
+        relative_next = parsed_next.path
+        if parsed_next.query:
+            relative_next += "?" + parsed_next.query
+        return relative_next
+    except ValueError:
+        # URL malformée (ex: IPv6 invalide), on ignore le paramètre next
+        return None
+
+
 @api_view(["GET"])
 @permission_classes([permissions.AllowAny])
 def oidc_login(request):
@@ -50,15 +62,8 @@ def oidc_logged_in(request):
 
     # gestion du `next` :
     if next_url := request.GET.get("next"):
-        try:
-            parsed_next = urlparse(next_url)
-            relative_next = parsed_next.path
-            if parsed_next.query:
-                relative_next += "?" + parsed_next.query
+        if relative_next := _sanitize_next_url(next_url):
             redirect_uri = add_url_params(redirect_uri, {"next": relative_next})
-        except ValueError:
-            # URL malformée (ex: IPv6 invalide), on ignore le paramètre next
-            pass
 
     # Passage au front des informations complémentaires de l'utilisateur
     # ici : SAFIR et / ou SIRET
