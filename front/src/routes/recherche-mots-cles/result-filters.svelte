90acb3a24251eb355faeac3646b9e2e57470261c
Louis <louis@gavalda.fr>
2026-06-26 18:59:49 +0200

    wip: keywords research results page

---
+++ front/src/routes/recherche-mots-cles/result-filters.svelte
@@ -0,0 +1,83 @@
+<script lang="ts" module>
+  import type {
+    FeeCondition,
+    FundingLabel,
+    LocationKind,
+    ServiceCategory,
+    ServiceKind,
+  } from "$lib/types";
+
+  // Variante mots-clés : mêmes filtres que la recherche par catégorie,
+  // PLUS un filtre « Thématiques et besoins » (cf. `categories` ci-dessous),
+  // la recherche par mots-clés n'ayant pas de catégorie présélectionnée.
+  export interface Filters {
+    categories: Array<ServiceCategory>;
+    diPublics: Array<string>;
+    kinds: Array<ServiceKind>;
+    fundingLabels: Array<FundingLabel["value"]>;
+    feeConditions: Array<FeeCondition>;
+    locationKinds: Array<LocationKind>;
+  }
+</script>
+
+<script lang="ts">
+  import type { ServicesOptions } from "$lib/types";
+
+  // Atome de filtre réutilisé tel quel depuis le contrôle (non couplé à PageData).
+  import ResultFilter from "../recherche/result-filter.svelte";
+
+  interface Props {
+    servicesOptions: ServicesOptions;
+    availableFundingLabels: Array<FundingLabel>;
+    filters: Filters;
+  }
+
+  let {
+    servicesOptions,
+    availableFundingLabels,
+    filters = $bindable(),
+  }: Props = $props();
+</script>
+
+<div class="gap-s32 flex flex-col">
+  <ResultFilter
+    id="categories"
+    label="Thématiques et besoins"
+    choices={servicesOptions.categories}
+    bind:group={filters.categories}
+  />
+  <ResultFilter
+    id="diPublics"
+    label="Publics"
+    choices={servicesOptions.diPublics}
+    bind:group={filters.diPublics}
+  />
+  <ResultFilter
+    id="kinds"
+    label="Type de service"
+    choices={servicesOptions.kinds}
+    bind:group={filters.kinds}
+  />
+  {#key availableFundingLabels}
+    {#if availableFundingLabels.length > 0}
+      <ResultFilter
+        id="fundingLabels"
+        label="Financé par"
+        choices={availableFundingLabels}
+        bind:group={filters.fundingLabels}
+      />
+    {/if}
+  {/key}
+  <ResultFilter
+    id="locationKinds"
+    label="Lieu d’accueil"
+    choices={servicesOptions.locationKinds}
+    bind:group={filters.locationKinds}
+  />
+  <ResultFilter
+    id="feeConditions"
+    label="Frais à charge"
+    choices={servicesOptions.feeConditions}
+    bind:group={filters.feeConditions}
+  />
+</div>
