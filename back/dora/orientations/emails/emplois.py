8023800f253e67e2467d86ad7499dcef3fc7b6af
David Martin Cohen <davidmartincohen@gmail.com>
2026-06-30 17:31:44 +0200

    refactor: déplacer tous les templates dans un sousdossier de l'app orientation

--- back/dora/orientations/emails/emplois.py
+++ back/dora/orientations/emails/emplois.py
@@ -47,7 +47,7 @@ def send_orientation_created(orientation):
         f"{'[Envoyée - Emplois Prescripteur] ' if debug else ''}Votre demande a bien été transmise !",
         orientation.prescriber_info.email,
         mjml2html(
-            render_to_string("emplois-orientation-creation-prescriber.mjml", context)
+            render_to_string("emplois/orientation-creation-prescriber.mjml", context)
         ),
         tags=["orientation"],
         reply_to=[orientation.get_contact_email()],
@@ -59,7 +59,7 @@ def send_orientation_created(orientation):
             f"{'[Envoyée - Emplois Conseiller référent] ' if debug else ''}Notification d’une demande d’orientation",
             orientation.referent_email,
             mjml2html(
-                render_to_string("emplois-orientation-creation-referent.mjml", context)
+                render_to_string("emplois/orientation-creation-referent.mjml", context)
             ),
             tags=["orientation"],
             reply_to=[orientation.prescriber_info.email],
@@ -70,7 +70,7 @@ def send_orientation_created(orientation):
         f"{'[Envoyée - Emplois Bénéficiaire] ' if debug else ''}Une orientation a été effectuée en votre nom",
         orientation.beneficiary_email,
         mjml2html(
-            render_to_string("emplois-orientation-creation-beneficiary.mjml", context)
+            render_to_string("emplois/orientation-creation-beneficiary.mjml", context)
         ),
         tags=["orientation"],
         reply_to=[orientation.prescriber_info.email],
@@ -94,7 +94,7 @@ def send_orientation_accepted(orientation, prescriber_message, beneficiary_messa
         f"{'[Validée - Emplois Prescripteur] ' if debug else ''}Votre demande a été acceptée ! 🎉",
         orientation.prescriber_info.email,
         mjml2html(
-            render_to_string("emplois-orientation-accepted-prescriber.mjml", context)
+            render_to_string("emplois/orientation-accepted-prescriber.mjml", context)
         ),
         tags=["orientation"],
         reply_to=[orientation.get_contact_email()],
@@ -106,7 +106,7 @@ def send_orientation_accepted(orientation, prescriber_message, beneficiary_messa
             f"{'[Validée - Emplois Conseiller référent] ' if debug else ''}Notification de l’acceptation d’une demande d’orientation",
             orientation.referent_email,
             mjml2html(
-                render_to_string("emplois-orientation-accepted-referent.mjml", context)
+                render_to_string("emplois/orientation-accepted-referent.mjml", context)
             ),
             tags=["orientation"],
             reply_to=[orientation.get_contact_email()],
@@ -117,7 +117,7 @@ def send_orientation_accepted(orientation, prescriber_message, beneficiary_messa
         f"{'[Validée - Emplois Bénéficiaire] ' if debug else ''}Votre demande a été acceptée ! 🎉",
         orientation.beneficiary_email,
         mjml2html(
-            render_to_string("emplois-orientation-accepted-beneficiary.mjml", context)
+            render_to_string("emplois/orientation-accepted-beneficiary.mjml", context)
         ),
         tags=["orientation"],
         reply_to=[orientation.get_contact_email()],
@@ -136,7 +136,7 @@ def send_orientation_rejected(orientation, message, other_details=""):
         f"{'[Refusée - Emplois Bénéficiaire] ' if debug else ''}Votre demande d’orientation a été refusée",
         orientation.beneficiary_email,
         mjml2html(
-            render_to_string("emplois-orientation-rejected-beneficiary.mjml", context)
+            render_to_string("emplois/orientation-rejected-beneficiary.mjml", context)
         ),
         tags=["orientation"],
         reply_to=[orientation.get_contact_email()],
@@ -147,7 +147,7 @@ def send_orientation_rejected(orientation, message, other_details=""):
         f"{'[Refusée - Emplois Prescripteur] ' if debug else ''}Votre demande d’orientation a été refusée",
         orientation.prescriber_info.email,
         mjml2html(
-            render_to_string("emplois-orientation-rejected-prescriber.mjml", context)
+            render_to_string("emplois/orientation-rejected-prescriber.mjml", context)
         ),
         tags=["orientation"],
         reply_to=[orientation.get_contact_email()],
@@ -160,7 +160,7 @@ def send_orientation_rejected(orientation, message, other_details=""):
             orientation.referent_email,
             mjml2html(
                 render_to_string(
-                    "emplois-orientation-rejected-prescriber.mjml", context
+                    "emplois/orientation-rejected-prescriber.mjml", context
                 )
             ),
             tags=["orientation"],
@@ -178,7 +178,7 @@ def send_orientation_expired(orientation, start_date=None):
         f"{'[Expirée - Emplois Prescripteur] ' if debug else ''}Votre demande d’orientation a expiré",
         orientation.prescriber_info.email,
         mjml2html(
-            render_to_string("emplois-orientation-expired-prescriber.mjml", context)
+            render_to_string("emplois/orientation-expired-prescriber.mjml", context)
         ),
         tags=["orientation"],
         reply_to=[orientation.get_contact_email()],
@@ -190,7 +190,7 @@ def send_orientation_expired(orientation, start_date=None):
             f"{'[Expirée - Emplois Conseiller référent] ' if debug else ''}Votre demande d’orientation a expiré",
             orientation.referent_email,
             mjml2html(
-                render_to_string("emplois-orientation-expired-prescriber.mjml", context)
+                render_to_string("emplois/orientation-expired-prescriber.mjml", context)
             ),
             tags=["orientation"],
             reply_to=[orientation.get_contact_email()],
@@ -201,7 +201,7 @@ def send_orientation_expired(orientation, start_date=None):
         f"{'[Expirée - Emplois Bénéficiaire] ' if debug else ''}Cette demande d’orientation a été annulée",
         orientation.beneficiary_email,
         mjml2html(
-            render_to_string("emplois-orientation-expired-beneficiary.mjml", context)
+            render_to_string("emplois/orientation-expired-beneficiary.mjml", context)
         ),
         tags=["orientation"],
         reply_to=[orientation.get_contact_email()],
@@ -221,7 +221,7 @@ def send_message_to_prescriber(orientation, message, cc):
         f"{'[Contact - Emplois Prescripteur] ' if debug else ''}Vous avez un nouveau message de la structure {structure_name}",
         orientation.prescriber_info.email,
         mjml2html(
-            render_to_string("emplois-orientation-contact-prescriber.mjml", context)
+            render_to_string("emplois/orientation-contact-prescriber.mjml", context)
         ),
         tags=["orientation"],
         reply_to=[orientation.get_contact_email()],
@@ -245,7 +245,7 @@ def send_message_to_beneficiary(orientation, message, cc):
         f"{'[Contact - Emplois Bénéficiaire] ' if debug else ''}Vous avez un nouveau message de la structure {structure_name}",
         orientation.beneficiary_email,
         mjml2html(
-            render_to_string("emplois-orientation-contact-beneficiary.mjml", context)
+            render_to_string("emplois/orientation-contact-beneficiary.mjml", context)
         ),
         tags=["orientation"],
         reply_to=[orientation.get_contact_email()],
