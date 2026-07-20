76a1040b0159391b4b4cd34a80f9370c16f0b60a
Gérald Gounot <gerald@gounot.eu>
2026-07-20 17:46:25 +0200

    chore(deps): Remplacement de la bibliothèque de génération de fichier Excel
    Remplacement de xlsx par write-excel-file :
    - accessible dans le registre npmjs.org ;
    - bundle plus léger.

--- front/src/lib/utils/spreadsheet.ts
+++ front/src/lib/utils/spreadsheet.ts
@@ -1,19 +1,19 @@
-import * as XLSX from "xlsx";
 import dayjs from "dayjs";
 
-export function generateSpreadsheet<DATA extends Array<Record<string, any>>>({
-  sheetData,
-  sheetName,
-}: {
-  sheetData: DATA;
-  sheetName: string;
-}) {
-  const numColumns = Object.keys(sheetData[0]).length;
+export async function generateSpreadsheet<
+  DATA extends Array<Record<string, any>>,
+>({ sheetData, sheetName }: { sheetData: DATA; sheetName: string }) {
+  // Import dynamique pour charger la lib uniquement côté navigateur et au moment de l'export
+  const { default: writeExcelFile } = await import("write-excel-file/browser");
 
-  const worksheet = XLSX.utils.json_to_sheet(sheetData);
-  worksheet["!cols"] = Array(numColumns).fill({ wch: 20 });
-  const workbook = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(workbook, worksheet);
+  const columns = Object.keys(sheetData[0]).map((header) => ({
+    header,
+    width: 20,
+    cell: (row: DATA[number]) => ({ value: row[header] }),
+  }));
+
   const date = dayjs().format("YYYY-MM-DD");
-  XLSX.writeFile(workbook, `${sheetName}-${date}.xlsx`, { compression: true });
+  await writeExcelFile(sheetData, { columns }).toFile(
+    `${sheetName}-${date}.xlsx`
+  );
 }
