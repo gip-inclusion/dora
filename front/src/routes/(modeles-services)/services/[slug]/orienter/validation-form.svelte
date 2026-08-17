4dff126149433834ff586bee16966743bb3e601d
David Martin Cohen <davidmartincohen@gmail.com>
2026-08-17 13:31:36 +0200

    refactor: supprimer valeurs obsolètes de computePublicsChoices()

--- front/src/routes/(modeles-services)/services/[slug]/orienter/validation-form.svelte
+++ front/src/routes/(modeles-services)/services/[slug]/orienter/validation-form.svelte
@@ -19,9 +19,8 @@
   let { service }: Props = $props();
 
   // Publics concernés par ce service
-  const { publicsChoices, publicsRequired } = computePublicsChoices(service);
-  orientationStep1Schema.situation.required = publicsRequired;
-  const serviceAcceptsAllPublic = publicsChoices.length === 0;
+  const { publicsChoices, servesAllPublics } = computePublicsChoices(service);
+  orientationStep1Schema.situation.required = !servesAllPublics;
 
   // Critères et conditions d’accès
   const { requirementChoices, requirementRequired } =
@@ -47,13 +46,13 @@
 
   <Fieldset title="Publics concernés par ce service" noTopPadding>
     <div class="lg:gap-s8 flex flex-col">
-      {#if serviceAcceptsAllPublic}
+      {#if servesAllPublics}
         <p class="mb-s0 italic">Ce service concerne tous les publics</p>
       {:else}
         <CheckboxesField
           id="situation"
           choices={publicsChoices}
-          description={!serviceAcceptsAllPublic
+          description={!servesAllPublics
             ? "Merci de cocher au moins un profil ou situation"
             : ""}
           bind:value={$orientation.situation}
