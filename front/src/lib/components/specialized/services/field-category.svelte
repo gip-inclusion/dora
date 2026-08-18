6a560e61304c393d9edb4ea6cc2c887381255e83
David Martin Cohen <davidmartincohen@gmail.com>
2026-08-18 18:36:26 +0200

    chore: supprimer inclusion numérique des catégories

--- front/src/lib/components/specialized/services/field-category.svelte
+++ front/src/lib/components/specialized/services/field-category.svelte
@@ -1,36 +1,20 @@
 <script lang="ts">
   import MultiSelectField from "$lib/components/forms/fields/multi-select-field.svelte";
   import type { Model, Service, ServicesOptions } from "$lib/types";
-  import InclusionNumSwitch from "./inclusion-num-switch.svelte";
 
   interface Props {
     servicesOptions: ServicesOptions;
     service: Service | Model;
-    model?: Model;
-    isModel?: boolean;
     description?: string;
   }
 
   let {
     servicesOptions,
     service = $bindable(),
-    model,
-    isModel = false,
     description = "",
   }: Props = $props();
-  const useModel = model != null;
 
   function handleCategoriesChange(categories) {
-    if (
-      service.categories.length === 0 &&
-      categories.length === 1 &&
-      categories[0] === "numerique"
-    ) {
-      service.useInclusionNumeriqueScheme = true;
-    }
-    if (categories.length !== 1) {
-      service.useInclusionNumeriqueScheme = false;
-    }
     service.categories = categories;
 
     service.subcategories = service.subcategories.filter((scat) =>
@@ -47,7 +31,3 @@
   sort
   {description}
 />
-
-{#if service.categories.includes("numerique") && !(isModel || useModel)}
-  <InclusionNumSwitch bind:service />
-{/if}
