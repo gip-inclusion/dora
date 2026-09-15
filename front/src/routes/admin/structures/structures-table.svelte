5753bb55a2d3a7c3ea1742f14e8c6c7281ab0ed4
Louis <louis@gavalda.fr>
2026-09-15 17:54:40 +0200

    feat: add data·inclusion banner at the end of structures list

--- front/src/routes/admin/structures/structures-table.svelte
+++ front/src/routes/admin/structures/structures-table.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { URL_MANAGER_DATA_INCLUSION_NOTICE } from "$lib/consts";
   import type { AdminStructure } from "$lib/types";
 
   import StructureCard from "./structure-card.svelte";
@@ -25,4 +26,22 @@
       {onRefresh}
     />
   {/each}
+
+  <aside class="border-info bg-info-light px-s20 py-s16 border-l-4">
+    <h3 class="text-f18 text-info mb-s8 leading-28">
+      Vous pouvez agir uniquement sur les structures et services créés via Dora
+    </h3>
+    <p class="text-f14 text-gray-text mb-s0 leading-24">
+      Vous voyez dans ce tableau de bord uniquement les services créés dans
+      Dora. Les visiteurs voient également les services issus de data·inclusion.
+      N’hésitez pas à nous signaler tout problème dans les données
+      data·inclusion en suivant
+      <a
+        href={URL_MANAGER_DATA_INCLUSION_NOTICE}
+        target="_blank"
+        rel="noopener"
+        class="underline">cette notice</a
+      >.
+    </p>
+  </aside>
 </div>
